66a07dee8582d8a18ebecc5fcc1b6f7bfe184c18
Daniel Greenfeld <pydanny@gmail.com>
2013-08-08 22:38:17 +0200

    Move logic out of command into more readily tested function

--- djstripe/management/commands/djstripe_init_plans.py
+++ djstripe/management/commands/djstripe_init_plans.py
@@ -1,8 +1,7 @@
 from __future__ import unicode_literals
-from django.conf import settings
 from django.core.management.base import BaseCommand
 
-import stripe
+from ...sync import sync_plans
 
 
 class Command(BaseCommand):
@@ -10,17 +9,4 @@ class Command(BaseCommand):
     help = "Make sure your Stripe account has the plans"
 
     def handle(self, *args, **options):
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        for plan in settings.DJSTRIPE_PLANS:
-            if settings.DJSTRIPE_PLANS[plan].get("stripe_plan_id"):
-                try:
-                    stripe.Plan.create(
-                        amount=settings.DJSTRIPE_PLANS[plan]["price"],
-                        interval=settings.DJSTRIPE_PLANS[plan]["interval"],
-                        name=settings.DJSTRIPE_PLANS[plan]["name"],
-                        currency=settings.DJSTRIPE_PLANS[plan]["currency"],
-                        id=settings.DJSTRIPE_PLANS[plan].get("stripe_plan_id")
-                    )
-                    print("Plan created for {0}".format(plan))
-                except Exception as e:
-                    print(e.message)
+        sync_plans()
