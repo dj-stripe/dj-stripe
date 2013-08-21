11fe1e4307a66afc7ff910a74704262ac58b0bfa
Daniel Greenfeld <pydanny@users.noreply.github.com>
2013-08-21 11:27:05 +0200

    Update with comments to clarify what we are doing.

--- djstripe/contrib/rest_framework/permissions.py
+++ djstripe/contrib/rest_framework/permissions.py
@@ -6,12 +6,19 @@ from ..models import Customer
 class DJStripePermission(BasePermission):
 
     def has_permission(self, request, view):
+        
         if request.user is None:
+            # No user? Then they don't have permission!
             return False
 
         # get the user's customer object
         customer, created = Customer.get_or_create(user=request.user)
+        
         if created:
+            # If just created, then they can't possibly have a subscription.
+            # Since customer.has_active_subscription() does at least one query,
+            #   we send them on their way without permission.
             return False
 
+        # Do formal check to see if user with permission has an active subscription.
         return customer.has_active_subscription()
