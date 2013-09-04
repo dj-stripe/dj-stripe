68668e6bf7ca74773934f313b8c88ccf341febcf
Luis Montiel <luismmontielg@gmail.com>
2013-09-04 11:58:47 -0700

    send_receipt available on charge() method

--- djstripe/models.py
+++ djstripe/models.py
@@ -514,7 +514,7 @@ class Customer(StripeObject):
             self.send_invoice()
         subscription_made.send(sender=self, plan=plan, stripe_response=resp)
 
-    def charge(self, amount, currency="usd", description=None):
+    def charge(self, amount, currency="usd", description=None, send_receipt=False):
         """
         This method expects `amount` to be a Decimal type representing a
         dollar amount. It will be converted to cents so any decimals beyond
@@ -531,7 +531,8 @@ class Customer(StripeObject):
             description=description,
         )
         obj = self.record_charge(resp["id"])
-        obj.send_receipt()
+        if send_receipt:
+            obj.send_receipt()
         return obj
 
     def record_charge(self, charge_id):
