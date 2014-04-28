ef37b67634178695244ff5daf23be41197b74a2c
Daniel Greenfeld <pydanny@gmail.com>
2014-04-28 10:43:51 -0700

    Bump version to 0.3.4

--- djstripe/__init__.py
+++ djstripe/__init__.py
@@ -4,7 +4,7 @@ __title__ = "dj-stripe"
 __summary__ = "Django + Stripe Made Easy"
 __uri__ = "https://github.com/pydanny/dj-stripe/"
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 
 __author__ = "Daniel Greenfeld"
 __email__ = "pydanny@gmail.com"
