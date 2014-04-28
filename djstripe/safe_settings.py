2d9022491bbc7168dd3bf3caf92cfcfc807532eb
Daniel Greenfeld <pydanny@gmail.com>
2014-04-28 12:06:40 -0700

    Place plans in a SortedDict so they don't change place all the time

--- djstripe/safe_settings.py
+++ djstripe/safe_settings.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from collections import OrderedDict
 
 from django.conf import settings
 
@@ -8,11 +9,20 @@ INVOICE_FROM_EMAIL = getattr(
     "DJSTRIPE_INVOICE_FROM_EMAIL",
     "billing@example.com"
 )
+
+# Get the PAYMENTS_PLANS dictionary
 PAYMENTS_PLANS = getattr(settings, "DJSTRIPE_PLANS", {})
+
+# Sort the PAYMENT_PLANS dictionary ascending by price.
+PAYMENT_PLANS = OrderedDict(sorted(PAYMENTS_PLANS.items(), key=lambda t: t[1]['price']))
+
 PLAN_CHOICES = [
     (plan, PAYMENTS_PLANS[plan].get("name", plan))
     for plan in PAYMENTS_PLANS
 ]
+#raise Exception(PLAN_CHOICES)
+#PLAN_CHOICES = sorted(PLAN_CHOICES, key=lambda x: x['price'])
+
 PASSWORD_INPUT_RENDER_VALUE = getattr(
     settings, 'DJSTRIPE_PASSWORD_INPUT_RENDER_VALUE', False)
 PASSWORD_MIN_LENGTH = getattr(
