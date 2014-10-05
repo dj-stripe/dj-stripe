0393e95f3920e3374ba8cf7c02ff644dcf623c42
Daniel Greenfeld <pydanny@users.noreply.github.com>
2014-10-05 11:13:25 -0700

    Update the publish command
    Why wouldn't we tag this every time?

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ except ImportError:
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     os.system('python setup.py bdist_wheel upload')
-    print("You probably want to also tag the version now:")
-    print("  git tag -a %s -m 'version %s'" % (version, version))
-    print("  git push --tags")
+    print("Tagging the version on github:")
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
     sys.exit()
 
 readme = open('README.rst').read()
