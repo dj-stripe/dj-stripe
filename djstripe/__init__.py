7a8b0ccd01f2b3ed77b225a4daab235e05018659
Daniel Greenfeld <danny@eventbrite.com>
2015-05-20 09:18:21 -0700

    Added Django 1.6.x or lower deprecation notice per #173

--- djstripe/__init__.py
+++ djstripe/__init__.py
@@ -1,4 +1,7 @@
 from __future__ import unicode_literals
+import warnings
+
+from django import get_version as get_django_version
 
 __title__ = "dj-stripe"
 __summary__ = "Django + Stripe Made Easy"
@@ -12,3 +15,9 @@ __email__ = "pydanny@gmail.com"
 __license__ = "BSD"
 __license__ = "License :: OSI Approved :: BSD License"
 __copyright__ = "Copyright 2015 Daniel Greenfeld"
+
+if get_django_version() <= '1.6.x':
+    msg = "dj-stripe deprecation notice: Django 1.6 and lower are deprecated\n" \
+            "and will be removed in dj-stripe 0.5.0.\n" \
+            "Reference: https://github.com/pydanny/dj-stripe/issues/173"
+    warnings.warn(msg)
