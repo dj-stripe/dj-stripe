e9e6bc9d9bfb76c62f4f0e2866b1083ebb1a98da
Philippe Luickx <philippe.luickx@gmail.com>
2015-05-27 14:29:50 +0300

    the restview for Subscriptions

---
+++ djstripe/serializers.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+# Python import
+
+# Django import
+# from django.utils.translation import ugettext_lazy as _
+
+# Contrib import
+from rest_framework.serializers import (
+    ModelSerializer,
+)
+from djstripe.models import CurrentSubscription
+from rest_framework import serializers
+
+# Project import
+
+
+class SubscriptionSerializer(ModelSerializer):
+
+    class Meta:
+        model = CurrentSubscription
+
+
+class CreateSubscriptionSerializer(serializers.Serializer):
+
+    stripe_token = serializers.CharField(max_length=200)
+    plan = serializers.CharField(max_length=200)
