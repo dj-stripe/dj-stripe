f5898e0ee22d61bfa800d9196166a2305088ee60
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2015-06-03 06:46:03 +0000

    pep8

--- djstripe/management/commands/djstripe_init_plans.py
+++ djstripe/management/commands/djstripe_init_plans.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from ...sync import sync_plans
