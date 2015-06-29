66e53da5216fc9a48e49e3ea0fb1b6443cd66aca
Chriss Mejía <chriss@chrissmejia.com>
2015-06-29 13:24:44 -0600

    [issue 201] support membership levels

--- djstripe/templatetags/djstripe_tags.py
+++ djstripe/templatetags/djstripe_tags.py
@@ -2,7 +2,7 @@
 from __future__ import division
 
 from django.template import Library
-
+from django.conf import settings
 
 register = Library()
 
@@ -22,3 +22,22 @@ def djdiv(value, arg):
         except Exception:
             return ''
 division.is_safe = False
+
+
+@register.filter(name='djstripe_plan_level')
+def djstripe_plan_level(name):
+    """
+    Add support to levels over plans, then you can have different kind of plans with the level same access.
+    
+    Use: {% <plan_name>|djstripe_plan_level %}
+
+    Custom settings setup need it, please see the documentation for details.
+    """
+    level = -1
+    hierarchy_plans = settings.DJSTRIPE_HIERARCHY_PLANS
+
+    for config_level in hierarchy_plans.values():
+        if name in config_level["plans"]:
+            level = config_level["level"]
+
+    return level
