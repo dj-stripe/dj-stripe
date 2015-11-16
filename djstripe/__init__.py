d3fb9e327107e1633386e361ec8af3b37b992b66
Michael Krens <mkrens@gmail.com>
2015-11-16 11:00:12 +0100

    fix unavailable django during installation

--- djstripe/__init__.py
+++ djstripe/__init__.py
@@ -1,8 +1,6 @@
 from __future__ import unicode_literals
 import warnings
 
-from django import get_version as get_django_version
-
 __title__ = "dj-stripe"
 __summary__ = "Django + Stripe Made Easy"
 __uri__ = "https://github.com/pydanny/dj-stripe/"
@@ -16,8 +14,14 @@ __license__ = "BSD"
 __license__ = "License :: OSI Approved :: BSD License"
 __copyright__ = "Copyright 2015 Daniel Greenfeld"
 
-if get_django_version() <= '1.6.x':
-    msg = "dj-stripe deprecation notice: Django 1.6 and lower are no longer\n" \
-        "supported. Please upgrade to Django 1.7 or higher.\n" \
-        "Reference: https://github.com/pydanny/dj-stripe/issues/173"
-    warnings.warn(msg)
+try:
+    from django import get_version as get_django_version
+
+    if get_django_version() <= '1.6.x':
+        msg = "dj-stripe deprecation notice: Django 1.6 and lower are no longer\n" \
+            "supported. Please upgrade to Django 1.7 or higher.\n" \
+            "Reference: https://github.com/pydanny/dj-stripe/issues/173"
+        warnings.warn(msg)
+except ImportError:
+    # during pip installation django might not be present yet
+    pass
