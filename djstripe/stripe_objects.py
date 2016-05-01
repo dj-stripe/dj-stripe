17fc50705c5eb6385b2caf7c1d4714e8b0cbe6f4
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2016-05-01 22:20:17 +0000

    added Card.create_token() (#162)

--- djstripe/stripe_objects.py
+++ djstripe/stripe_objects.py
@@ -922,6 +922,33 @@ class StripeCard(StripeSource):
         if "account" in data and data["account"]:
             return target_cls.get_or_create_from_stripe_object(data, "account")[0]
 
+    @classmethod
+    def create_token(self, number, exp_month, exp_year, cvc, **kwargs):
+        """
+        Creates a single use token that wraps the details of a credit card. This token can be used in
+        place of a credit card dictionary with any API method. These tokens can only be used once: by
+        creating a new charge object, or attaching them to a customer.
+        (Source: https://stripe.com/docs/api/python#create_card_token)
+
+        :param exp_month: The card's expiration month.
+        :type exp_month: Two digit int
+        :param exp_year: The card's expiration year.
+        :type exp_year: Two or Four digit int
+        :param number: The card number
+        :type number: string without any separators (no spaces)
+        :param cvc: Card security code.
+        :type cvc: string
+        """
+
+        card = {
+            "number": number,
+            "exp_month": exp_month,
+            "exp_year": exp_year,
+            "cvc": cvc,
+        }
+        card.update(kwargs)
+
+        return stripe.Token.create(card=card)
 
 # ============================================================================ #
 #                                Subscriptions                                 #
