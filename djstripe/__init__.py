69f0d5a397c0a88de4c5921ee6a2d68108c0c38d
Filip <peccator93@gmail.com>
2016-08-11 20:03:50 +0200

    Proper django version checking
    Closes #346

--- djstripe/__init__.py
+++ djstripe/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 import warnings
 
-from django import get_version as get_django_version
+from django import VERSION as django_version
 
 __title__ = "dj-stripe"
 __summary__ = "Django + Stripe Made Easy"
@@ -16,7 +16,7 @@ __license__ = "BSD"
 __license__ = "License :: OSI Approved :: BSD License"
 __copyright__ = "Copyright 2016 Alexander Kavanaugh"
 
-if get_django_version() <= '1.7.x':
+if django_version < (1, 8):
     msg = "dj-stripe deprecation notice: Django 1.7 and lower are no longer\n" \
         "supported. Please upgrade to Django 1.8 or higher.\n" \
         "Reference: https://github.com/kavdev/dj-stripe/issues/275"
