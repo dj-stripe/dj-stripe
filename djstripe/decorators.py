f8f7b4b62e88212248d5b6f62c47d9dcbf31e0b0
Alec Brunelle <alec@alec.coffee>
2016-11-30 19:45:19 -0500

    Change decorator doc based on feedback

--- djstripe/decorators.py
+++ djstripe/decorators.py
@@ -22,7 +22,7 @@ def subscriber_passes_pay_test(test_func, plan=None, pay_page="djstripe:subscrib
     """
     Decorator for views that checks the subscriber passes the given test for a "Paid Feature".
 
-    Redirects to the pay_page if necessary. The test should be a callable
+    Redirects to the `pay_page` if necessary. The test should be a callable
     that takes the subscriber object and returns True if the subscriber passes.
     """
     def decorator(view_func):
@@ -40,7 +40,7 @@ def subscription_payment_required(function=None, plan=None, pay_page="djstripe:s
     """
     Decorator for views that require subscription payment.
 
-    Redirects to the pay_page if necessary.
+    Redirects to the `pay_page` if necessary.
     """
     actual_decorator = subscriber_passes_pay_test(
         subscriber_has_active_subscription,
