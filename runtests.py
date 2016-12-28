632b80eecbe6de0928dc05b926701d66c0ace07e
Alexander Kavanaugh <alex@kavdev.io>
2016-12-28 14:50:51 -0800

    Moving migrations exclusion to setup.cfg

--- runtests.py
+++ runtests.py
@@ -223,8 +223,7 @@ def run_test_suite(args):
         print("----------------------------------------------------------------------")
 
         from subprocess import call
-        flake_result = call(["flake8", ".", "--count", "--exclude",
-                             ".venv,djstripe/migrations"])
+        flake_result = call(["flake8", ".", "--count"])
         if flake_result != 0:
             sys.stderr.write("pep8 errors detected.\n")
             sys.stderr.write(colored(text="\nYOUR CHANGES HAVE INTRODUCED PEP8 ERRORS!\n\n",
