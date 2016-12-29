c5b2bc623437bcb19deda4116de433cb3d97cf83
Alexander Kavanaugh <alex@kavdev.io>
2016-12-29 00:52:52 +0000

    pep8 fixes

--- djstripe/models.py
+++ djstripe/models.py
@@ -931,6 +931,7 @@ class EventProcessingException(TimeStampedModel):
             event=self.event
         ))
 
+
 # Much like registering signal handlers. We import this module so that its registrations get picked up
 # the NO QA directive tells flake8 to not complain about the unused import
 from . import event_handlers  # NOQA
