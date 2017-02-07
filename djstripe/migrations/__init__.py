25b1ea5d8118b9fae932c4e5d403632ab1e52500
Jerome Leclanche <jerome@leclan.ch>
2017-02-07 11:47:05 +0200

    Remove old reference to South

--- djstripe/migrations/__init__.py
+++ djstripe/migrations/__init__.py
@@ -1,18 +1,1 @@
-"""
-Migrations have been built for Django=>1.7 versions. Alternative migrations
-for Django<1.7 users are provided with the ``south_migrations`` dir.
 
-"""
-
-SOUTH_ERROR_MESSAGE = """\n
-For South support, customize the SOUTH_MIGRATION_MODULES setting like so:
-    SOUTH_MIGRATION_MODULES = {
-        'djstripe': 'djstripe.south_migrations',
-    }
-"""
-
-try:
-    from django.db import migrations  # noqa
-except ImportError:
-    from django.core.exceptions import ImproperlyConfigured
-    raise ImproperlyConfigured(SOUTH_ERROR_MESSAGE)
