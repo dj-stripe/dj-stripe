dj-stripe/dj-stripe

commit 448ecc1bf26732c3d501f06f9a0a5365e0364e79

Upgrade/downgrade of subscriptions (fix #32) and improved nav (fix #25)

djstripe/templatetags/djstripe_tags.py
@@ -1,0 +1,19 @@
+from __future__ import division
+
+from django.template import Library
+
+
+register = Library()
+
+
+@register.filter
+def division(value, arg):
+    """Divide the arg by the value."""
+    try:
+        return value / arg
+    except (ValueError, TypeError):
+        try:
+            return value / arg
+        except Exception:
+            return ''
+division.is_safe = False

djstripe/templates/djstripe/account.html
@@ -63,7 +63,7 @@
                                 <td>{{ plan.description }}</td>
                                 <td>
                                     {% if plan.plan == subscription.plan %}
-                                        Your plan
+                                        <strong>Your plan</strong>
                                     {% else %}
                                         Not your plan
                                     {% endif %}
@@ -103,15 +103,15 @@
 
 {% block javascript %}
 {{ block.super }}
-<script>
-    $(function() {
-        $.post("{% url 'djstripe:sync_history' %}", function(data) {
-            $('#history-table').html(data);
+{% if plans %}
+    <script>
+        $(function() {
+            $.post("{% url 'djstripe:sync_history' %}", function(data) {
+                $('#history-table').html(data);
+            });
         });
-    });
-</script>
-
-{% if not plans %}
+    </script>
+{% else %}
     <script>
         $(function() {
             $.post("{% url 'djstripe:sync_history' %}", function(data) {

djstripe/templates/djstripe/history.html
@@ -3,7 +3,10 @@
 {% block title %}Payment History{% endblock title %}
 
 {% block content %}
-    {{ block.super }}
+    <ul class="breadcrumb">
+      <li><a href="{% url 'djstripe:account' %}">Home</a></li>
+      <li class="active">Payment History</li>
+    </ul>
     {% include "djstripe/includes/_history_table.html" %}
 {% endblock content %}
 

djstripe/templates/djstripe/includes/_history_table.html
@@ -1,39 +1,41 @@
 {% load static %}
 <div id="history-table">
-<h2>Payment History</h2>
-{% if customer.invoices.all %}
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Transaction ID</th>
-                <th>Date</th>
-                <th>Amount</th>
-                <th class="pull-right"><img class="in-progress-gif" src="{% static 'img/in-progress.gif' %}" /></th>
-            </tr>
-        </thead>
-        {% for invoice in customer.invoices.all %}
-            <tr>
-                <td class="fixed">#{{ invoice.id }}</td>
-                <td>{{ invoice.date|date:"M d" }}</td>
-                </td>
-                <td>
-                    {% if invoice.paid and invoice.total > 0 %}
-                        <p>
-                            <span class="label label-success">Paid</span> ${{ invoice.total|floatformat:"2" }}
-                        </p>
-                    {% endif %}
-                    
-                    {% if invoice.paid and invoice.total < 0 %}
-                        <p>
-                            <span class="label label-success">Credit</span> ${{ invoice.total|floatformat:"2" }}
-                        </p>
-                    {% endif %}
-                </td>
-                <td></td>
-            </tr>
-        {% endfor %}
-    </table>
-{% else %}
-    <p>You have not made any payments yet.</p>
-{% endif %}
+<h2>Payment History <img class="in-progress-gif" src="{% static 'img/in-progress.gif' %}" /></h2>
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>Transaction ID</th>
+            <th>Date</th>
+            <th>Amount</th>
+            
+        </tr>
+    </thead>
+    {% for invoice in customer.invoices.all %}
+        <tr>
+            <td class="fixed">#{{ invoice.id }}</td>
+            <td>{{ invoice.date|date:"M d" }}</td>
+            </td>
+            <td colspan=2>
+                {% if invoice.paid and invoice.total > 0 %}
+                    <p>
+                        <span class="label label-success">Paid</span> ${{ invoice.total|floatformat:"2" }}
+                    </p>
+                {% endif %}
+                
+                {% if invoice.paid and invoice.total < 0 %}
+                    <p>
+                        <span class="label label-success">Credit</span> ${{ invoice.total|floatformat:"2" }}
+                    </p>
+                {% endif %}
+            </td>
+        </tr>
+    {% empty %}
+        <tr>
+            <td colspan="3">
+                <img class="in-progress-gif" src="{% static 'img/in-progress.gif' %}" />
+            </td>
+        </tr>
+    {% endfor %}
+</table>
+
 </div>

djstripe/templates/djstripe/subscribe_form.html
@@ -1,10 +1,14 @@
 {% extends "djstripe/base.html" %}
-{% load crispy_forms_tags static %}
+{% load crispy_forms_tags static djstripe_tags %}
 
 {% block title %}Choose a Subscription{% endblock title %}
 
 {% block content %}
 {{ block.super }}
+<ul class="breadcrumb">
+  <li><a href="{% url 'djstripe:account' %}">Home</a></li>
+  <li class="active">Subscription</li>
+</ul>
 <h2>Choose a Subscription</h2>
 
 {% if error %}
@@ -17,7 +21,14 @@
 <div class="row">
     {% for plan in PLAN_LIST %}
         <div class="col-4">
-            <form action="{% url 'djstripe:subscribe' %}" class="djstripe-payments"  data-stripe-key="{{ STRIPE_PUBLIC_KEY }}"  method="POST">
+            <form
+              {% if not customer.current_subscription %}
+                  action="{% url 'djstripe:subscribe' %}" class="djstripe-subscribe"  
+              {% else %}
+                  action="{% url 'djstripe:change_plan' %}" class="djstripe-change-plan"
+              {% endif %}
+
+              data-stripe-key="{{ STRIPE_PUBLIC_KEY }}"  method="POST">
                 {% csrf_token %}
                 <input type="hidden" name="plan" value="{{ plan.plan }}" />
                 <input name="stripe_token" type="hidden" /> 
@@ -34,8 +45,15 @@
                   <h3>{{ plan.name }}</h3>
                   <p>{{ plan.description }}</p>
                 </button>
-              {% if customer.current_subscription.plan == plan.plan %}
+
+              {% if not customer.current_subscription %}
+                <!-- do nothing -->
+              {% elif customer.current_subscription.plan == plan.plan %}
                 <h4>Your Current Plan</h4>
+              {% elif customer.current_subscription.amount < plan.price|division:100 %}
+                <h4>Upgrade</h4>
+              {% elif customer.current_subscription.amount > plan.price|division:100 %}
+                <h4>Downgrade</h4>
               {% endif %}
             </form>
         </div>
@@ -49,7 +67,7 @@
 <script src="//checkout.stripe.com/v2/checkout.js"></script>
 <script text="text/javascript">
     $(function() {
-        $('body').on("click", '.djstripe-payments button[type=submit]', function(e) {
+        $('body').on("click", '.djstripe-subscribe button[type=submit]', function(e) {
           e.preventDefault();
           var $form = $(".djstripe-payments"),
               token = function(res) {
@@ -70,6 +88,14 @@
 
           return false;
         });
+        $('.djstripe-change-plan').click(function(e){
+            $("button[type=submit]").attr("disabled", "true");
+            $('#in-progress').modal({"keyboard": false})
+            $('.progress-bar').animate({width:'+=100%'}, 2000);
+            var $form = $(".djstripe-change-plan")
+            $form.trigger("submit");
+        });
+
     });
 </script>
 {% endblock javascript %}

djstripe/urls.py
@@ -34,6 +34,11 @@ urlpatterns = patterns("",
         name="subscribe"
     ),
     url(
+        r"^change/pan/$",
+        views.ChangePlanView.as_view(),
+        name="change_plan"
+    ),
+    url(
         r"^change/card/$",
         views.ChangeCardView.as_view(),
         name="change_card"

djstripe/viewmixins.py
@@ -40,3 +40,11 @@ class PaymentsContextMixin(object):
             "PAYMENT_PLANS": app_settings.PAYMENTS_PLANS
         })
         return context
+
+
+class SubscriptionMixin(PaymentsContextMixin):
+    def get_context_data(self, *args, **kwargs):
+        context = super(SubscriptionMixin, self).get_context_data(**kwargs)
+        context['is_plans_plural'] = bool(len(app_settings.PLAN_CHOICES) > 1)
+        context['customer'], created = Customer.get_or_create(self.request.user)
+        return context

djstripe/views.py
@@ -10,6 +10,7 @@ from django.views.generic import TemplateView
 from django.views.generic import View
 
 from braces.views import CsrfExemptMixin
+from braces.views import FormValidMessageMixin
 from braces.views import LoginRequiredMixin
 from braces.views import SelectRelatedMixin
 import stripe
@@ -19,52 +20,13 @@ from .models import CurrentSubscription
 from .models import Customer
 from .models import Event
 from .models import EventProcessingException
-from .settings import PLAN_CHOICES
 from .settings import PLAN_LIST
 from .settings import PY3
 from .sync import sync_customer
-from .viewmixins import PaymentsContextMixin
+from .viewmixins import PaymentsContextMixin, SubscriptionMixin
 
 # This is here as a utility. Might move it out.
 from .viewmixins import SubscriptionPaymentRequiredMixin
-
-
-class SubscribeFormView(
-        LoginRequiredMixin,
-        PaymentsContextMixin,
-        FormView):
-    # TODO - needs tests
-
-    form_class = PlanForm
-    template_name = "djstripe/subscribe_form.html"
-    success_url = reverse_lazy("djstripe:history")
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(SubscribeFormView, self).get_context_data(**kwargs)
-        context['is_plans_plural'] = bool(len(PLAN_CHOICES) > 1)
-        context['customer'], created = Customer.get_or_create(self.request.user)
-        return context
-
-    def post(self, request, *args, **kwargs):
-        """
-        Handles POST requests, instantiating a form instance with the passed
-        POST variables and then checked for validity.
-        """
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        if form.is_valid():
-            try:
-                customer, created = Customer.get_or_create(self.request.user)
-                customer.update_card(self.request.POST.get("stripe_token"))
-                customer.subscribe(form.cleaned_data["plan"])
-            except stripe.StripeError as e:
-                # add form error here
-                self.error = e.args[0]
-                return self.form_invalid(form)
-            # redirect to confirmation page
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
 
 
 class ChangeCardView(LoginRequiredMixin, PaymentsContextMixin, TemplateView):
@@ -140,3 +102,64 @@ class AccountView(LoginRequiredMixin, SelectRelatedMixin, TemplateView):
             context['subscription'] = None
         context['plans'] = PLAN_LIST
         return context
+
+
+################## Subscription views
+
+
+class SubscribeFormView(
+        LoginRequiredMixin,
+        FormValidMessageMixin,
+        SubscriptionMixin,
+        FormView):
+    # TODO - needs tests
+
+    form_class = PlanForm
+    template_name = "djstripe/subscribe_form.html"
+    success_url = reverse_lazy("djstripe:history")
+    form_valid_message = "You are now subscribed!"
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handles POST requests, instantiating a form instance with the passed
+        POST variables and then checked for validity.
+        """
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        if form.is_valid():
+            try:
+                customer, created = Customer.get_or_create(self.request.user)
+                customer.update_card(self.request.POST.get("stripe_token"))
+                customer.subscribe(form.cleaned_data["plan"])
+            except stripe.StripeError as e:
+                # add form error here
+                self.error = e.args[0]
+                return self.form_invalid(form)
+            # redirect to confirmation page
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+
+class ChangePlanView(LoginRequiredMixin,
+                        FormValidMessageMixin,
+                        SubscriptionMixin,
+                        FormView):
+
+    form_class = PlanForm
+    template_name = "djstripe/subscribe_form.html"
+    success_url = reverse_lazy("djstripe:history")
+    form_valid_message = "You've just changed your plan!"
+
+    def post(self, request, *args, **kwargs):
+        form = PlanForm(request.POST)
+        customer = request.user.customer
+        if form.is_valid():
+            try:
+                customer.subscribe(form.cleaned_data["plan"])
+            except stripe.StripeError as e:
+                self.error = e.message
+                return self.form_invalid(form)
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)

commit d32c6731f24f05158fad0f817b2c1c201cbb0b77

Fix a renaming issue in djdiv templatetag
djdiv was initially called division, introduced in 448ecc1b.
The is_safe setting was not touched when it was renamed.

djstripe/templatetags/djstripe_tags.py
@@ -14,7 +14,7 @@ from .. import settings as djstripe_settings
 register = Library()
 
 
-@register.filter
+@register.filter(is_safe=False)
 def djdiv(value, arg):
     """
     Divide the value by the arg, using Python 3-style division that returns floats.
@@ -28,9 +28,6 @@ def djdiv(value, arg):
             return value / arg
         except Exception:
             return ''
-
-
-division.is_safe = False
 
 
 @register.filter(name='djstripe_plan_level')