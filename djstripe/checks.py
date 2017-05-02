6bd9a6701bffa134f33b078f40c0f54f6618a04f
Lee Skillen <lskillen@cloudsmith.io>
2017-05-02 10:39:31 +0100

    Minor fixes from code review

--- djstripe/checks.py
+++ djstripe/checks.py
@@ -20,31 +20,31 @@ from django.core.exceptions import ImproperlyConfigured
 def check_stripe_api_key(app_configs=None, **kwargs):
     """Check the user has configured API live/test keys correctly."""
     from . import settings as djstripe_settings
-    msgs = []
+    messages = []
 
     if not djstripe_settings.STRIPE_SECRET_KEY:
         msg = "Could not find a Stripe API key."
         hint = "Add STRIPE_TEST_SECRET_KEY and STRIPE_LIVE_SECRET_KEY to your settings."
-        msgs.append(checks.Critical(msg, hint=hint, id="djstripe.C001"))
+        messages.append(checks.Critical(msg, hint=hint, id="djstripe.C001"))
     elif djstripe_settings.STRIPE_LIVE_MODE:
         if not djstripe_settings.LIVE_API_KEY.startswith("sk_live_"):
             msg = "Bad Stripe live API key."
             hint = 'STRIPE_LIVE_SECRET_KEY should start with "sk_live_"'
-            msgs.append(checks.Critical(msg, hint=hint, id="djstripe.C002"))
+            messages.append(checks.Critical(msg, hint=hint, id="djstripe.C002"))
     else:
         if not djstripe_settings.TEST_API_KEY.startswith("sk_test_"):
             msg = "Bad Stripe test API key."
             hint = 'STRIPE_TEST_SECRET_KEY should start with "sk_test_"'
-            msgs.append(checks.Critical(msg, hint=hint, id="djstripe.C003"))
+            messages.append(checks.Critical(msg, hint=hint, id="djstripe.C003"))
 
-    return msgs
+    return messages
 
 
 @checks.register("djstripe")
 def check_stripe_api_version(app_configs=None, **kwargs):
     """Check the user has configured API version correctly."""
     from . import settings as djstripe_settings
-    msgs = []
+    messages = []
     default_version = djstripe_settings.DEFAULT_STRIPE_API_VERSION
     version = djstripe_settings.get_stripe_api_version()
 
@@ -52,14 +52,15 @@ def check_stripe_api_version(app_configs=None, **kwargs):
         djstripe_settings.check_stripe_api_version(version)
     except ImproperlyConfigured as ex:
         hint = "Use a valid date string value."
-        msgs.append(checks.Critical(six.force_text(ex), hint=hint, id="djstripe.C004"))
+        messages.append(checks.Critical(six.force_text(ex), hint=hint, id="djstripe.C004"))
 
     if version != default_version:
         msg = (
             "The Stripe API version has a non-default value of '{}'. "
             "Non-default versions are not explicitly supported, and may "
-            "cause compatibility issues.".format(version))
+            "cause compatibility issues.".format(version)
+        )
         hint = "Use the dj-stripe default for Stripe API version: {}".format(default_version)
-        msgs.append(checks.Warning(msg, hint=hint, id="djstripe.W001"))
+        messages.append(checks.Warning(msg, hint=hint, id="djstripe.W001"))
 
-    return msgs
+    return messages
