92ebaf9e1507acf1fc10f4448fc00db43508f23b
Jerome Leclanche <jerome@leclan.ch>
2017-05-22 21:11:20 +0300

    Allow alternate string key lookup for enums
    This lets us use syntactically-incorrect strings as keys for enum
    values, such as "pass" or "Diners Club".

--- djstripe/enums.py
+++ djstripe/enums.py
@@ -5,7 +5,15 @@ from django.utils.decorators import classproperty
 class Enum(_Enum):
     @classproperty
     def choices(cls):
-        return tuple((k, v.value) for k, v in cls.__members__.items())
+        return tuple((cls.keys.get(k, k), v.value) for k, v in cls.__members__.items())
+
+    @classproperty
+    def keys(cls):
+        # Returns a mapping of key overrides.
+        # This allows using syntactically-incorrect values as keys,
+        # such as keywords ("pass") or spaces ("Diners Club").
+        # This cannot be an attribute, otherwise it would show up as a choice.
+        return {}
 
 
 class CardTokenizationMethod(Enum):
