3b54569137fe7e8ea2f8182b9930200ad48dc808
Jerome Leclanche <jerome@leclan.ch>
2017-06-05 18:27:43 +0300

    New approach using DjangoChoices

--- djstripe/enums.py
+++ djstripe/enums.py
@@ -1,104 +1,78 @@
-from enum import Enum as _Enum
-from django.utils.decorators import classproperty
 from django.utils.translation import ugettext as _
+from djchoices import C, DjangoChoices
 
 
-class Enum(_Enum):
-    @classproperty
-    def choices(cls):
-        return tuple((cls.keys.get(k, k), v.value) for k, v in cls.__members__.items())
-
-    @classproperty
-    def keys(cls):
-        # Returns a mapping of key overrides.
-        # This allows using syntactically-incorrect values as keys,
-        # such as keywords ("pass") or spaces ("Diners Club").
-        # This cannot be an attribute, otherwise it would show up as a choice.
-        return {}
-
-
-class ApiErrorCode(Enum):
+class ApiErrorCode(DjangoChoices):
     """
     Charge failure error codes.
 
     https://stripe.com/docs/api#error-codes
     """
 
-    invalid_number = _("Invalid number")
-    invalid_expiry_month = _("Invalid expiration month")
-    invalid_expiry_year = _("Invalid expiration year")
-    invalid_cvc = _("Invalid security code")
-    invalid_swipe_data = _("Invalid swipe data")
-    incorrect_number = _("Incorrect number")
-    expired_card = _("Expired card")
-    incorrect_cvc = _("Incorrect security code")
-    incorrect_zip = _("ZIP code failed validation")
-    card_declined = _("Card was declined")
-    missing = _("No card being charged")
-    processing_error = _("Processing error")
+    invalid_number = C(_("Invalid number"), "invalid_number")
+    invalid_expiry_month = C(_("Invalid expiration month"), "invalid_expiry_month")
+    invalid_expiry_year = C(_("Invalid expiration year"), "invalid_expiry_year")
+    invalid_cvc = C(_("Invalid security code"), "invalid_cvc")
+    invalid_swipe_data = C(_("Invalid swipe data"), "invalid_swipe_data")
+    incorrect_number = C(_("Incorrect number"), "incorrect_number")
+    expired_card = C(_("Expired card"), "expired_card")
+    incorrect_cvc = C(_("Incorrect security code"), "incorrect_cvc")
+    incorrect_zip = C(_("ZIP code failed validation"), "incorrect_zip")
+    card_declined = C(_("Card was declined"), "card_declined")
+    missing = C(_("No card being charged"), "missing")
+    processing_error = C(_("Processing error"), "processing_error")
 
 
-class CardCheckResult(Enum):
-    pass_ = _("Pass")
-    fail = _("Fail")
-    unavailable = _("Unavailable")
-    unchecked = _("Unchecked")
-
-    @classproperty
-    def keys(cls):
-        return {"pass_": "pass"}
+class CardCheckResult(DjangoChoices):
+    pass_ = C(_("Pass"), "pass")
+    fail = C(_("Fail"), "fail")
+    unavailable = C(_("Unavailable"), "unavailable")
+    unchecked = C(_("Unchecked"), "unchecked")
 
 
-class CardBrand(Enum):
-    Visa = _("Visa")
-    AmericanExpress = _("American Express")
-    MasterCard = _("MasterCard")
-    Discover = _("Discover")
-    JCB = _("JCB")
-    DinersClub = _("Diners Club")
-    Unknown = _("Unknown")
-
-    @classproperty
-    def keys(cls):
-        return {
-            "AmericanExpress": "American Express",
-            "DinersClub": "Diners Club",
-        }
+class CardBrand(DjangoChoices):
+    Visa = C(_("Visa"), "Visa")
+    AmericanExpress = C(_("American Express"), "American Express")
+    MasterCard = C(_("MasterCard"), "MasterCard")
+    Discover = C(_("Discover"), "Discover")
+    JCB = C(_("JCB"), "JCB")
+    DinersClub = C(_("Diners Club"), "Diners Club")
+    Unknown = C(_("Unknown"), "Unknown")
 
 
-class CardFundingType(Enum):
-    credit = _("Credit")
-    debit = _("Debit")
-    prepaid = _("Prepaid")
-    unknown = _("Unknown")
+class CardFundingType(DjangoChoices):
+    credit = C(_("Credit"), "credit")
+    debit = C(_("Debit"), "debit")
+    prepaid = C(_("Prepaid"), "prepaid")
+    unknown = C(_("Unknown"), "unknown")
 
 
-class CardTokenizationMethod(Enum):
-    apple_pay = _("Apple Pay")
-    android_pay = _("Android Pay")
+class CardTokenizationMethod(DjangoChoices):
+    apple_pay = C(_("Apple Pay"), "apple_pay")
+    android_pay = C(_("Android Pay"), "android_pay")
 
 
-class ChargeStatus(Enum):
-    succeeded = _("Succeeded")
-    pending = _("Pending")
-    failed = _("Failed")
+class ChargeStatus(DjangoChoices):
+    succeeded = C(_("Succeeded"), "succeeded")
+    pending = C(_("Pending"), "pending")
+    failed = C(_("Failed"), "failed")
 
 
-class CouponDuration(Enum):
-    once = _("Once")
-    repeating = _("Multi-month")
-    forever = _("Forever")
+class CouponDuration(DjangoChoices):
+    once = C(_("Once"), "once")
+    repeating = C(_("Multi-month"), "repeating")
+    forever = C(_("Forever"), "forever")
 
 
-class PlanInterval(Enum):
-    day = _("Day")
-    week = _("Week")
-    month = _("Month")
-    year = _("Year")
+class PlanInterval(DjangoChoices):
+    day = C(_("Day"), "day")
+    week = C(_("Week"), "week")
+    month = C(_("Month"), "month")
+    year = C(_("Year"), "year")
 
 
-class SourceType(Enum):
-    card = _("Card")
-    bank_account = _("Bank account")
-    bitcoin_receiver = _("Bitcoin receiver")
-    alipay_account = _("Alipay account")
+class SourceType(DjangoChoices):
+    card = C(_("Card"), "card")
+    bank_account = C(_("Bank account"), "bank_account")
+    bitcoin_receiver = C(_("Bitcoin receiver"), "bitcoin_receiver")
+    alipay_account = C(_("Alipay account"), "alipay_account")
