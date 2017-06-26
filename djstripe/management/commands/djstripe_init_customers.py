c55dc919c88ff3734e6c64a3e58b2656f66672f8
tolo <tolo.trm@gmail.com>
2017-06-26 22:45:34 +0200

    fix command issues with customer does not exists

--- djstripe/management/commands/djstripe_init_customers.py
+++ djstripe/management/commands/djstripe_init_customers.py
@@ -22,7 +22,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Create Customer objects for Subscribers without Customer objects associated."""
-        for subscriber in get_subscriber_model().objects.filter(customer__isnull=True):
+        for subscriber in get_subscriber_model().objects.filter(djstripe_customers=None):
             # use get_or_create in case of race conditions on large subscriber bases
             Customer.get_or_create(subscriber=subscriber)
             print("Created subscriber for {0}".format(subscriber.email))
