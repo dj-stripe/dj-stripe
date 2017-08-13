457536223225d0687a70713464665e9184af915c
Jerome Leclanche <jerome@leclan.ch>
2017-08-13 03:50:02 +0300

    Add isort and run it on flake8 tests

--- djstripe/models.py
+++ djstripe/models.py
@@ -1121,4 +1121,4 @@ class EventProcessingException(models.Model):
 
 # Much like registering signal handlers. We import this module so that its registrations get picked up
 # the NO QA directive tells flake8 to not complain about the unused import
-from . import event_handlers  # NOQA
+from . import event_handlers  # NOQA, isort:skip
