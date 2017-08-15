d6c96da8dde80c40d3f431260db60e8df23ae1a8
Jerome Leclanche <jerome@leclan.ch>
2017-08-15 14:37:06 +0300

    Allow importing models.* as stripe_objects.Stripe* for backwards compat

--- djstripe/stripe_objects.py
+++ djstripe/stripe_objects.py
@@ -3,8 +3,21 @@
 import warnings
 
 from .models import *  # noqa, isort:skip
+from . import models
 
 warnings.warn(
     "djstripe.stripe_objects is a deprecated module, please use djstripe.models",
     DeprecationWarning
 )
+
+StripeCharge = models.Charge
+StripeCustomer = models.Customer
+StripeEvent = models.Event
+StripePayout = models.Payout
+StripeCard = models.Card
+StripeCoupon = models.Coupon
+StripeInvoice = models.Invoice
+StripePlan = models.Plan
+StripeSubscription = models.Subscription
+StripeAccount = models.Account
+StripeTransfer = models.Transfer
