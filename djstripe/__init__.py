b1ea830a4ab9630ae89373090b0bde59c1b4f5a2
Jerome Leclanche <jerome@leclan.ch>
2018-01-09 03:59:19 +0200

    Set dj-stripe as stripe app info
    https://stripe.com/docs/building-plugins#setappinfo
    (cherry picked from commit a17933c7806634391137244e2c17327898187146)

--- djstripe/__init__.py
+++ djstripe/__init__.py
@@ -5,7 +5,19 @@
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 import pkg_resources
+
+import stripe
+
 from . import checks  # noqa: Register the checks
 
 
 __version__ = pkg_resources.require("dj-stripe")[0].version
+
+
+# Set app info
+# https://stripe.com/docs/building-plugins#setappinfo
+stripe.set_app_info(
+    "dj-stripe",
+    version=__version__,
+    url="https://github.com/dj-stripe/dj-stripe"
+)
