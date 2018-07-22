dc092d8374f613ba2916f55d673445286bac0ca5
Jerome Leclanche <jerome@leclan.ch>
2018-07-22 21:17:19 +0300

    Remove a Python2-related import alias

--- djstripe/enums.py
+++ djstripe/enums.py
@@ -1,7 +1,7 @@
 import operator
 from collections import OrderedDict
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class EnumMetaClass(type):
