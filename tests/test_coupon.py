a0faab49ec2e555b4e6c247ee337d54f9764b747
Benedict Etzel <developer@beheh.de>
2018-10-18 21:18:39 +0200

    Change Coupon.percent_off to be a StripePercentField
    [2018-07-27][1] changed the percent_off field on coupons to be a float,
    which means we can just use the StripePercentField here in future.
    
    [1]: https://stripe.com/docs/upgrades#2018-07-27

--- tests/test_coupon.py
+++ tests/test_coupon.py
@@ -40,32 +40,40 @@ class HumanReadableCouponTest(TestCase):
 
     def test_human_readable_percent_off_forever(self):
         coupon = Coupon.objects.create(
-            id="coupon-test-percent-off-forever", percent_off=10, currency="usd",
+            id="coupon-test-percent-off-forever", percent_off=10.25, currency="usd",
             duration="forever",
         )
-        self.assertEqual(coupon.human_readable, "10% off forever")
+        self.assertEqual(coupon.human_readable, "10.25% off forever")
         self.assertEqual(str(coupon), coupon.human_readable)
 
     def test_human_readable_percent_off_once(self):
         coupon = Coupon.objects.create(
-            id="coupon-test-percent-off-once", percent_off=10, currency="usd",
+            id="coupon-test-percent-off-once", percent_off=10.25, currency="usd",
             duration="once",
         )
-        self.assertEqual(coupon.human_readable, "10% off once")
+        self.assertEqual(coupon.human_readable, "10.25% off once")
         self.assertEqual(str(coupon), coupon.human_readable)
 
     def test_human_readable_percent_off_one_month(self):
         coupon = Coupon.objects.create(
-            id="coupon-test-percent-off-1month", percent_off=10, currency="usd",
+            id="coupon-test-percent-off-1month", percent_off=10.25, currency="usd",
             duration="repeating", duration_in_months=1,
         )
-        self.assertEqual(coupon.human_readable, "10% off for 1 month")
+        self.assertEqual(coupon.human_readable, "10.25% off for 1 month")
         self.assertEqual(str(coupon), coupon.human_readable)
 
     def test_human_readable_percent_off_three_months(self):
         coupon = Coupon.objects.create(
-            id="coupon-test-percent-off-3month", percent_off=10, currency="usd",
+            id="coupon-test-percent-off-3month", percent_off=10.25, currency="usd",
             duration="repeating", duration_in_months=3,
         )
-        self.assertEqual(coupon.human_readable, "10% off for 3 months")
+        self.assertEqual(coupon.human_readable, "10.25% off for 3 months")
+        self.assertEqual(str(coupon), coupon.human_readable)
+
+    def test_human_readable_integer_percent_off_forever(self):
+        coupon = Coupon.objects.create(
+            id="coupon-test-percent-off-forever", percent_off=10, currency="usd",
+            duration="forever",
+        )
+        self.assertEqual(coupon.human_readable, "10% off forever")
         self.assertEqual(str(coupon), coupon.human_readable)
