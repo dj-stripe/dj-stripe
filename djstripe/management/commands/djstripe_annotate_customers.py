11ebd0337a66bc477ff200533f20d66ba0e29a17
Kelly Storm <kstorm@blackboxoperations.com>
2019-01-21 05:41:32 -0500

    adjustments to satisfy CI/CD requirements, restrictions

--- djstripe/management/commands/djstripe_annotate_customers.py
+++ djstripe/management/commands/djstripe_annotate_customers.py
@@ -1,13 +1,14 @@
 """
 annotate_customers command.
 """
-import datetime 
+import datetime
 
 from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist
 
 from ...models import Customer
 from ...settings import get_subscriber_model
+
 
 class Command(BaseCommand):
 	"""Annotate customer instances within the Stripe account itself."""
@@ -17,10 +18,10 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		"""Annotate customer accounts within remote Stripe environments by
 		adding or changing the djstripe_subscriber value within the account's metadata
-		field, to the subscriber.id value, if and when a subscriber/customer pair does exist, 
+		field, to the subscriber.id value, if and when a subscriber/customer pair does exist,
 		but the customer information isn't availably locally and the subscriber information isn't
 		attached to the customer account information remotely."""
-		
+
 		for customer in Customer.api_list():
 			print("Annotating Customer: %s " % customer.id)
 
@@ -28,10 +29,10 @@ class Command(BaseCommand):
 				dj_subscriber = get_subscriber_model().objects.get(email=customer.email)
 				if dj_subscriber:
 					customer.metadata = {} or customer.metadata
-					customer.metadata['djstripe_subscriber'] = dj_subscriber.id 
+					customer.metadata['djstripe_subscriber'] = dj_subscriber.id
 					customer.metadata['updated'] = datetime.datetime.now()
 					customer.save()
 			except ObjectDoesNotExist as e:
-				# these will be created by the djstripe_init_customers command 
+				# these will be created by the djstripe_init_customers command
 				print("%s %s" % (customer.email, e))
-				continue 
+				continue
