67b0fe529afbc2b7833bf3696112d24526761d60
John Carter <john@therefromhere.org>
2019-02-27 17:31:28 +1300

    Add migration hack to support sqlite

--- djstripe/migrations/0003_auto_20181117_2328.py
+++ djstripe/migrations/0003_auto_20181117_2328.py
@@ -7,6 +7,19 @@ import djstripe.fields
 
 
 class Migration(migrations.Migration):
+	def __init__(self, *args, **kwargs):
+		try:
+			# Hack to support sqlite for quick testing. Without this migrations fail with:
+			#    "django.db.utils.NotSupportedError: Renaming the 'djstripe_paymentmethod' table while in a transaction
+			#    is not supported on SQLite because it would break referential integrity.
+			#    Try adding `atomic = False` to the Migration class."
+			from django.db import connection
+
+			if connection.vendor == "sqlite":
+				self.atomic = False
+		except Exception:
+			pass
+		super().__init__(*args, **kwargs)
 
 	dependencies = [("djstripe", "0002_auto_20180627_1121")]
 
