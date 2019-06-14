4b671d411599eea97bfd000e9e5cd713b1526027
John Carter <john@therefromhere.org>
2019-06-14 22:17:09 +1200

    DEFAULT_STRIPE_API_VERSION = "2019-02-11"

--- djstripe/settings.py
+++ djstripe/settings.py
@@ -9,7 +9,7 @@ from django.utils.module_loading import import_string
 
 from .checks import validate_stripe_api_version
 
-DEFAULT_STRIPE_API_VERSION = "2018-11-08"
+DEFAULT_STRIPE_API_VERSION = "2019-02-11"
 
 
 def get_callback_function(setting_name, default=None):
