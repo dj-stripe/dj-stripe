a3ef7bb85a49909918fcfaaf042308f295b543bc
John Carter <john@therefromhere.org>
2019-06-22 20:38:40 +1200

    Add a simple Subscription create view to the example app

---
+++ tests/apps/example/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+app_name = "djstripe_example"
+
+urlpatterns = [
+	path(
+		"purchase-subscription",
+		views.PurchaseSubscriptionView.as_view(),
+		name="purchase_subscription",
+	),
+	path(
+		"purchase-subscription-success/<id>",
+		views.PurchaseSubscriptionSuccessView.as_view(),
+		name="purchase_subscription_success",
+	),
+]
