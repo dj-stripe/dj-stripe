a3434e5f78a52a2eb09eea9ac1bde1e318bb41e3
John Carter <john@therefromhere.org>
2019-12-14 17:52:37 +1300

    fix migration

--- djstripe/migrations/0012_djstripeupcominginvoicetotaltaxamount.py
+++ djstripe/migrations/0012_djstripeupcominginvoicetotaltaxamount.py
@@ -41,7 +41,7 @@ class Migration(migrations.Migration):
                     "invoice",
                     models.ForeignKey(
                         on_delete=django.db.models.deletion.CASCADE,
-                        related_name="total_tax_amounts",
+                        related_name="+",
                         to="djstripe.UpcomingInvoice",
                     ),
                 ),
