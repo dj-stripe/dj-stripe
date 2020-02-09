7bb91fd01ca1cb72617ff75d895e4671067399dc
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-02-09 21:18:02 +0100

    Special override of delete method.

--- djstripe/contrib/rest_framework/views.py
+++ djstripe/contrib/rest_framework/views.py
@@ -148,6 +148,18 @@ class SubscriptionDetailView(RetrieveUpdateAPIView):
     permission_classes = (IsAuthenticated, IsSubscriptionOwner)
     serializer_class = SubscriptionSerializer
 
+    def delete(self, request, *args, **kwargs):
+        # To stick to Stripe way of doing, we must enable the DELETE method to cancel
+        # a subscription. Bur the default implementation truly deletes the object.
+        # Hence we override it here to simulate a partial update (PATCH).
+        # See https://stackoverflow.com/a/21262262/707984 for explanations about
+        # _mutable.
+        mutable = request.data._mutable
+        request.data._mutable = True
+        request.data.update(status=SubscriptionStatus.canceled)
+        request.data._mutable = mutable
+        return self.partial_update(request, *args, **kwargs)
+
 
 class PlanListView(ListAPIView):
     queryset = Plan.objects.all()
