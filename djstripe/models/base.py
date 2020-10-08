8533b2877e6a1d9266f53c1e9c2a6a4ff41e6528
John Vandenberg <jayvdb@gmail.com>
2020-10-08 12:46:43 +0700

    base: Get stripe_account for Stripe API methods

--- djstripe/models/base.py
+++ djstripe/models/base.py
@@ -94,6 +94,48 @@ class StripeModel(models.Model):
     def default_api_key(self):
         return djstripe_settings.get_default_api_key(self.livemode)
 
+    def _get_stripe_account_id(self, api_key=None):
+        """
+        Call the stripe API's retrieve operation for this model.
+
+        :param api_key: The api key to use for this request. \
+            Defaults to settings.STRIPE_SECRET_KEY.
+        :type api_key: string
+        :param stripe_account: The optional connected account \
+            for which this request is being made.
+        :type stripe_account: string
+        """
+        api_key = api_key or self.default_api_key
+
+        try:
+            djstripe_owner_account = self.djstripe_owner_account
+            if djstripe_owner_account is not None:
+                return djstripe_owner_account.id
+        except (AttributeError, KeyError, ValueError):
+            pass
+
+        # Get reverse foreign key relations to Account in case we need to
+        # retrieve ourselves using that Account ID.
+        reverse_account_relations = (
+            field
+            for field in self._meta.get_fields(include_parents=True)
+            if field.is_relation and field.one_to_many
+            # Avoid circular import problems by using the app registry to
+            # get the model class rather than a direct import.
+            and field.related_model
+            is apps.get_model(app_label="djstripe", model_name="account")
+        )
+
+        # Handle case where we have a reverse relation to Account and should pass
+        # that account ID to the retrieve call.
+        for field in reverse_account_relations:
+            # Grab the related object, using the first one we find.
+            reverse_lookup_attr = field.get_accessor_name()
+            account = getattr(self, reverse_lookup_attr).first()
+
+            if account is not None:
+                return account.id
+
     def api_retrieve(self, api_key=None, stripe_account=None):
         """
         Call the stripe API's retrieve operation for this model.
@@ -108,27 +150,7 @@ class StripeModel(models.Model):
         api_key = api_key or self.default_api_key
         # Prefer passed in stripe_account if set.
         if not stripe_account:
-            # Get reverse foreign key relations to Account in case we need to
-            # retrieve ourselves using that Account ID.
-            reverse_account_relations = (
-                field
-                for field in self._meta.get_fields(include_parents=True)
-                if field.is_relation and field.one_to_many
-                # Avoid circular import problems by using the app registry to
-                # get the model class rather than a direct import.
-                and field.related_model
-                is apps.get_model(app_label="djstripe", model_name="account")
-            )
-
-            # Handle case where we have a reverse relation to Account and should pass
-            # that account ID to the retrieve call.
-            for field in reverse_account_relations:
-                # Grab the related object, using the first one we find.
-                reverse_lookup_attr = field.get_accessor_name()
-                account = getattr(self, reverse_lookup_attr).first()
-                if account is not None:
-                    stripe_account = account.id
-                    break
+            stripe_account = self._get_stripe_account_id(api_key)
 
         return self.stripe_class.retrieve(
             id=self.id,
@@ -177,9 +199,33 @@ class StripeModel(models.Model):
         :type stripe_account: string
         """
         api_key = api_key or self.default_api_key
+        # Prefer passed in stripe_account if set.
+        if not stripe_account:
+            stripe_account = self._get_stripe_account_id(api_key)
 
         return self.api_retrieve(api_key=api_key, stripe_account=stripe_account).delete(
             **kwargs
+        )
+
+    def _api_update(self, api_key=None, stripe_account=None, **kwargs):
+        """
+        Call the stripe API's modify operation for this model
+
+        :param api_key: The api key to use for this request.
+            Defaults to djstripe_settings.STRIPE_SECRET_KEY.
+        :type api_key: string
+        :param stripe_account: The optional connected account \
+            for which this request is being made.
+        :type stripe_account: string
+        """
+        api_key = api_key or self.default_api_key
+        # Prefer passed in stripe_account if set.
+        if not stripe_account:
+            stripe_account = self._get_stripe_account_id(api_key)
+
+        instance = self.api_retrieve(api_key=api_key, stripe_account=stripe_account)
+        return instance.request(
+            "post", instance.instance_url(), params=kwargs
         )
 
     def str_parts(self):
