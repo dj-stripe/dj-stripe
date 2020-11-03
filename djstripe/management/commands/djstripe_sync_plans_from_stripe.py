1a498d33165e5990bd705c65f9da3857366f0144
Jerome Leclanche <jerome@leclan.ch>
2020-11-03 10:53:38 +0100

    Refactor djstripe_sync_plans_from_stripe to sync prices as well
    Closes #1258

--- djstripe/management/commands/djstripe_sync_plans_from_stripe.py
+++ djstripe/management/commands/djstripe_sync_plans_from_stripe.py
@@ -3,16 +3,19 @@ sync_plans_from_stripe command.
 """
 from django.core.management.base import BaseCommand
 
-from ...models import Plan
+from ...models import Plan, Price
 
 
 class Command(BaseCommand):
-    """Sync plans from stripe."""
+    """Sync prices (and plans) from stripe."""
 
-    help = "Sync plans from stripe."
+    help = "Sync prices (and plans) from stripe."
 
     def handle(self, *args, **options):
-        """Call sync_from_stripe_data for each plan returned by api_list."""
+        for price_data in Price.api_list():
+            price = Price.sync_from_stripe_data(price_data)
+            self.stdout.write(f"Synchronized price {price.id}")
+
         for plan_data in Plan.api_list():
             plan = Plan.sync_from_stripe_data(plan_data)
-            print("Synchronized plan {0}".format(plan.id))
+            self.stdout.write(f"Synchronized plan {plan.id}")
