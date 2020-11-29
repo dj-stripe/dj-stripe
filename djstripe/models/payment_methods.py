66dad7f7ee07bb1c3c22117b6715600a44737719
Pierre Dulac <dulacpier@gmail.com>
2020-11-29 20:01:46 +0100

    Fix sync model recursion loop
    * Add failing test for sync customer with default payment method
    
    The test fails with a RecursionError after retrieving the payment method
    details, which then fetches customer details and it goes like this in an
    infinite recursive loop
    
    * Fix failing test test_customer_sync_default_payment_method_string
    
    I reused the same mechanism used to avoid other recursion loops, by
    using the current_ids list of already fetched entities.

--- djstripe/models/payment_methods.py
+++ djstripe/models/payment_methods.py
@@ -473,8 +473,10 @@ class Source(StripeModel):
         data["source_data"] = data[data["type"]]
         return data
 
-    def _attach_objects_hook(self, cls, data):
-        customer = cls._stripe_object_to_customer(target_cls=Customer, data=data)
+    def _attach_objects_hook(self, cls, data, current_ids=None):
+        customer = cls._stripe_object_to_customer(
+            target_cls=Customer, data=data, current_ids=current_ids
+        )
         if customer:
             self.customer = customer
         else:
@@ -610,8 +612,13 @@ class PaymentMethod(StripeModel):
         help_text="Additional information for payment methods of type `sofort`",
     )
 
-    def _attach_objects_hook(self, cls, data):
-        customer = cls._stripe_object_to_customer(target_cls=Customer, data=data)
+    def _attach_objects_hook(self, cls, data, current_ids=None):
+        customer = None
+        if current_ids is None or data.get("customer") not in current_ids:
+            customer = cls._stripe_object_to_customer(
+                target_cls=Customer, data=data, current_ids=current_ids
+            )
+
         if customer:
             self.customer = customer
         else:
