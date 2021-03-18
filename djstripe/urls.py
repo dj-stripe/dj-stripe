831ee6902b6c5ac663029e9615ca0ae65b4703ed
Jerome Leclanche <jerome@leclan.ch>
2021-03-18 22:35:27 +0100

    Remove outdated comment
    Supercedes #1330

--- djstripe/urls.py
+++ djstripe/urls.py
@@ -6,14 +6,6 @@ Wire this into the root URLConf this way::
     path("stripe/", include("djstripe.urls", namespace="djstripe")),
     # url can be changed
     # Call to 'djstripe.urls' and 'namespace' must stay as is
-
-Call it from reverse()::
-
-    reverse("djstripe:subscribe")
-
-Call from url tag::
-
-    {% url "djstripe:subscribe" %}
 """
 from django.urls import re_path
 
