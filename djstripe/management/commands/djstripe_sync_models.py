ace77b25e1d72c706088f5937240659c8d9c04ee
Arnav Choudhury <arnav13@gmail.com>
2021-06-25 18:30:14 +0530

    Fixed various bugs in get_list_kwargs() method
    get_list_kwargs() was neither fetching the class attribute expand_fields nor was able to correclty unpack them. Added and/or updated the way list_kwargs dictionary was constructed for models like PaymentMethod, SubscriptionItem, TransferReversal, ApplicationFeeRefund, TaxId, and UsageRecord to work with their specific custom list methods.
    
    Updated the module to skip over model instances that threw any error. Earlier behaviour was to stop and skip the entire model class even if any 1 instance throw an error.
    
    Added ability to sync ApplicationFee, ApplicationFeeRefund, Transfer, TransferReversal, and TaxId models.
    
    Also added ability to sync Card and Bank Account objects  by both Customers and External Accounts. Earlier they were being synced only by their respective External Accounts, thereby, leaving out any Card and/or BankAccount instance only having a not null Customer model field.

--- djstripe/management/commands/djstripe_sync_models.py
+++ djstripe/management/commands/djstripe_sync_models.py
@@ -51,6 +51,13 @@ class Command(BaseCommand):
             return False, "no stripe_class"
 
         if not hasattr(model.stripe_class, "list"):
+            # todo add UsageRecordSummary
+            if model in (
+                models.ApplicationFeeRefund,
+                models.TransferReversal,
+                models.TaxId,
+            ):
+                return True, ""
             return False, "no stripe_class.list"
 
         if model is models.UpcomingInvoice:
@@ -67,7 +74,7 @@ class Command(BaseCommand):
 
         should_sync, reason = self._should_sync_model(model)
         if not should_sync:
-            self.stdout.write(f"Skipping {model}: {reason}")
+            self.stderr.write(f"Skipping {model}: {reason}")
             return
 
         self.stdout.write("Syncing {}:".format(model_name))
@@ -92,32 +99,41 @@ class Command(BaseCommand):
                     )
 
                     # syncing BankAccount and Card objects of Stripe Connected Express and Custom Accounts
-                    self.sync_external_account_bank_accounts_and_cards(djstripe_obj)
+                    self.sync_bank_accounts_and_cards(djstripe_obj)
 
                 for stripe_obj in model.api_list(**list_kwargs):
-                    count += 1
-                    djstripe_obj = model.sync_from_stripe_data(stripe_obj)
-                    self.stdout.write(
-                        "  id={id}, pk={pk} ({djstripe_obj})".format(
-                            id=djstripe_obj.id,
-                            pk=djstripe_obj.pk,
-                            djstripe_obj=djstripe_obj,
+                    # Skip model instances that throw an error
+                    try:
+                        djstripe_obj = model.sync_from_stripe_data(stripe_obj)
+                        self.stdout.write(
+                            "  id={id}, pk={pk} ({djstripe_obj})".format(
+                                id=djstripe_obj.id,
+                                pk=djstripe_obj.pk,
+                                djstripe_obj=djstripe_obj,
+                            )
                         )
+                        # syncing BankAccount and Card objects of Stripe Connected Express and Custom Accounts
+                        self.sync_bank_accounts_and_cards(djstripe_obj)
+                        count += 1
+                    except Exception as e:
+                        self.stderr.write(f"Skipping {stripe_obj.get('id')}: {e}")
+                        continue
+
+            if count == 0:
+                self.stdout.write("  (no results)")
+            else:
+                self.stdout.write(
+                    "  Synced {count} {model_name}".format(
+                        count=count, model_name=model_name
                     )
-                    # syncing BankAccount and Card objects of Stripe Connected Express and Custom Accounts
-                    self.sync_external_account_bank_accounts_and_cards(djstripe_obj)
+                )
 
         except Exception as e:
             self.stderr.write(str(e))
 
-        if count == 0:
-            self.stdout.write("  (no results)")
-        else:
-            self.stdout.write(
-                "  Synced {count} {model_name}".format(
-                    count=count, model_name=model_name
-                )
-            )
+    # todo Handle syncing data for connected accounts as well. # https://stripe.com/docs/api/accounts/list
+    # ! Will need to re-run with different values of stripe_account
+    # todo handle supoorting double + nested fields like data.invoice.subscriptions.customer etc?
 
     def get_list_kwargs(self, model):
         """
@@ -130,61 +146,100 @@ class Command(BaseCommand):
         """
         all_list_kwargs = (
             [{"expand": [f"data.{k}" for k in model.expand_fields]}]
-            if getattr(models, "expand_fields", [])
-            else []
+            if getattr(model, "expand_fields", [])
+            else [{}]
         )
         if model is models.PaymentMethod:
             # special case
-            all_list_kwargs.extend(
-                (
-                    {"customer": stripe_customer.id, "type": "card"}
-                    for stripe_customer in models.Customer.api_list()
-                )
-            )
+            all_list_kwargs = [
+                {"customer": stripe_customer.id, "type": "card", **all_list_kwargs[0]}
+                for stripe_customer in models.Customer.api_list()
+            ]
         elif model is models.SubscriptionItem:
-            all_list_kwargs.extend(
-                (
-                    {"subscription": subscription.id}
-                    for subscription in models.Subscription.api_list()
-                )
-            )
+            all_list_kwargs = [
+                {"subscription": subscription.id, **all_list_kwargs[0]}
+                for subscription in models.Subscription.api_list()
+            ]
+
         elif model is models.CountrySpec:
             all_list_kwargs.extend(({"limit": 50},))
+
+        elif model is models.TransferReversal:
+            all_list_kwargs = [
+                {"id": transfer.id, **all_list_kwargs[0]}
+                for transfer in models.Transfer.api_list()
+            ]
+
+        elif model is models.ApplicationFeeRefund:
+            all_list_kwargs = [
+                {"id": fee.id, **all_list_kwargs[0]}
+                for fee in models.ApplicationFee.api_list()
+            ]
+        elif model is models.TaxId:
+            all_list_kwargs = [
+                {"id": customer.id, **all_list_kwargs[0]}
+                for customer in models.Customer.api_list()
+            ]
+
+        elif model is models.UsageRecord:
+            all_list_kwargs = [
+                {"id": subscription_item.id, **all_list_kwargs[0]}
+                for subscription in models.Subscription.api_list()
+                for subscription_item in models.SubscriptionItem.api_list(
+                    subscription=subscription.id
+                )
+            ]
         elif not all_list_kwargs:
             all_list_kwargs.append({})
 
         return all_list_kwargs
 
-    def sync_external_account_bank_accounts_and_cards(self, instance):
+    def sync_bank_accounts_and_cards(self, instance):
+        """
+        Syncs Bank Accounts and Cards for both customers and all external accounts
+        """
         type = getattr(instance, "type", None)
-        id = instance.id
+        kwargs = {
+            "id": instance.id,
+            "api_key": djstripe_settings.STRIPE_SECRET_KEY,
+        }
 
         if type in (enums.AccountType.custom, enums.AccountType.express) and isinstance(
             instance, models.Account
         ):
+
             # fetch all Card and BankAccount objects associated with the instance
-            items = models.Account.stripe_class.list_external_accounts(
-                id,
-                api_key=djstripe_settings.STRIPE_SECRET_KEY,
+            items = models.Account.stripe_class.list_external_accounts(**kwargs)
+
+            self.start_sync(items, instance)
+        elif isinstance(instance, models.Customer):
+            for object in ("card", "bank_account"):
+                kwargs["object"] = object
+
+                # fetch all Card and BankAccount objects associated with the instance
+                items = models.Customer.stripe_class.list_sources(**kwargs)
+
+                self.start_sync(items, instance)
+
+    def start_sync(self, items, instance):
+        bank_count = 0
+        card_count = 0
+        for item in items.auto_paging_iter():
+
+            if item.object == "bank_account":
+                model = models.BankAccount
+                bank_count += 1
+            elif item.object == "card":
+                model = models.Card
+                card_count += 1
+
+            item_obj = model.sync_from_stripe_data(item)
+
+            self.stdout.write(
+                f"\tSyncing {model._meta.verbose_name} ({instance}): id={item_obj.id}, pk={item_obj.pk}"
             )
-            bank_count = 0
-            card_count = 0
-            for item in items.auto_paging_iter():
 
-                if item.object == "bank_account":
-                    model = models.BankAccount
-                    bank_count += 1
-                elif item.object == "card":
-                    model = models.Card
-                    card_count += 1
-
-                item_obj = model.sync_from_stripe_data(item)
-
-                self.stdout.write(
-                    f"\tSyncing {model._meta.verbose_name} ({instance}): id={item_obj.id}, pk={item_obj.pk}"
-                )
-
-            if bank_count + card_count > 0:
-                self.stdout.write(
-                    f"\tSynced {bank_count} BankAccounts and {card_count} Cards"
-                )
+        if bank_count + card_count > 0:
+            self.stdout.write(
+                f"\tSynced {bank_count} BankAccounts and {card_count} Cards"
+            )
