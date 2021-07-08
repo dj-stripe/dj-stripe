d82c25a6be5559d94c170fe960307ecdc11e163e
Arnav Choudhury <arnav13@gmail.com>
2021-07-08 12:50:29 +0530

    Updated TaxId with custom methods to retrieve, list, and create TaxId objects
    Also updated TaxId.__str__ to better reflect what is displayed on the Stripe Dashboard.

--- djstripe/models/billing.py
+++ djstripe/models/billing.py
@@ -25,6 +25,7 @@ from ..managers import SubscriptionManager
 from ..settings import djstripe_settings
 from ..utils import QuerySetMock, get_friendly_currency_amount
 from .base import StripeModel
+from .core import Customer
 
 
 # TODO Add Tests
@@ -86,6 +87,16 @@ class DjstripeUpcomingInvoiceTotalTaxAmount(models.Model):
 
 
 class Coupon(StripeModel):
+    """
+    A coupon contains information about a percent-off or amount-off discount you might want to apply to a customer.
+    Coupons may be applied to invoices or orders.
+    Coupons do not work with conventional one-off charges.
+
+    Stripe documentation: https://stripe.com/docs/api/coupons
+    """
+
+    stripe_class = stripe.Coupon
+
     id = StripeIdField(max_length=500)
     amount_off = StripeDecimalCurrencyAmountField(
         null=True,
@@ -1807,6 +1818,14 @@ class SubscriptionSchedule(StripeModel):
 
 # TODO Add Tests
 class TaxId(StripeModel):
+    """
+    Add one or multiple tax IDs to a customer.
+    A customer's tax IDs are displayed on invoices and
+    credit notes issued for the customer.
+
+    Stripe documentation: https://stripe.com/docs/api/customer_tax_ids
+    """
+
     stripe_class = stripe.TaxId
     description = None
     metadata = None
@@ -1825,27 +1844,70 @@ class TaxId(StripeModel):
     verification = JSONField(help_text="Tax ID verification information.")
 
     def __str__(self):
-        return self.value
+        return f"{enums.TaxIdType.humanize(self.type)} {self.value} ({self.verification.get('status')})"
 
     class Meta:
         verbose_name = "Tax ID"
         verbose_name_plural = "Tax IDs"
 
+    @classmethod
+    def _api_create(cls, api_key=djstripe_settings.STRIPE_SECRET_KEY, **kwargs):
+        """
+        Call the stripe API's create operation for this model.
+
+        :param api_key: The api key to use for this request. \
+            Defaults to djstripe_settings.STRIPE_SECRET_KEY.
+        :type api_key: string
+        """
+
+        if not kwargs.get("id"):
+            raise KeyError("Customer Object ID is missing")
+
+        try:
+            Customer.objects.get(id=kwargs["id"])
+        except Customer.DoesNotExist:
+            raise
+
+        return stripe.Customer.create_tax_id(api_key=api_key, **kwargs)
+
     def api_retrieve(self, api_key=None, stripe_account=None):
+        """
+        Call the stripe API's retrieve operation for this model.
+        :param api_key: The api key to use for this request. \
+            Defaults to djstripe_settings.STRIPE_SECRET_KEY.
+        :type api_key: string
+        :param stripe_account: The optional connected account \
+            for which this request is being made.
+        :type stripe_account: string
+        """
+        nested_id = self.id
+        id = self.customer.id
+
+        # Prefer passed in stripe_account if set.
         if not stripe_account:
             stripe_account = self._get_stripe_account_id(api_key)
 
-        customer = self.customer.api_retrieve(
-            api_key=api_key or self.default_api_key,
-            stripe_account=stripe_account,
-        )
-        return customer.retrieve_tax_id(
-            customer.id,
-            self.id,
+        return stripe.Customer.retrieve_tax_id(
+            id=id,
+            nested_id=nested_id,
             api_key=api_key or self.default_api_key,
             expand=self.expand_fields,
             stripe_account=stripe_account,
         )
+
+    @classmethod
+    def api_list(cls, api_key=djstripe_settings.STRIPE_SECRET_KEY, **kwargs):
+        """
+        Call the stripe API's list operation for this model.
+        :param api_key: The api key to use for this request. \
+            Defaults to djstripe_settings.STRIPE_SECRET_KEY.
+        :type api_key: string
+        See Stripe documentation for accepted kwargs for each object.
+        :returns: an iterator over all items in the query
+        """
+        return stripe.Customer.list_tax_ids(
+            api_key=api_key, **kwargs
+        ).auto_paging_iter()
 
 
 class TaxRate(StripeModel):
