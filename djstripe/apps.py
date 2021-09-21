f2daabb5f54e95546db76b3aa0172417073d2c73
Arnav Choudhury <arnav13@gmail.com>
2021-09-21 15:58:10 +0530

    Added an apps.py submodule to load the appconfig as recommended by django
    This is why webhook registrations were not getting picked up correctly by tox (pytest) in djangomain

---
+++ djstripe/apps.py
@@ -0,0 +1,33 @@
+"""
+dj-stripe - Django + Stripe Made Easy
+"""
+import pkg_resources
+from django.apps import AppConfig
+
+__version__ = pkg_resources.get_distribution("dj-stripe").version
+
+
+class DjstripeAppConfig(AppConfig):
+    """
+    An AppConfig for dj-stripe which loads system checks
+    and event handlers once Django is ready.
+    """
+
+    name = "djstripe"
+    default_auto_field = "django.db.models.AutoField"
+
+    def ready(self):
+        import stripe
+
+        from . import (  # noqa: Register the checks and event handlers
+            checks,
+            event_handlers,
+        )
+
+        # Set app info
+        # https://stripe.com/docs/building-plugins#setappinfo
+        stripe.set_app_info(
+            "dj-stripe",
+            version=__version__,
+            url="https://github.com/dj-stripe/dj-stripe",
+        )
