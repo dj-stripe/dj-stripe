f8de867a55dc7b0254677fae67334c61c079b9c6
Arnav Choudhury <arnav13@gmail.com>
2021-11-27 14:18:22 +0530

    Added the deleted_on_stripe key to the Customer data dict
    This is done so that Customers can be updated locally depending on whether they have been deleted upstream or not.

--- djstripe/models/core.py
+++ djstripe/models/core.py
@@ -738,6 +738,17 @@ class Customer(StripeModel):
 
     @classmethod
     def _manipulate_stripe_object_hook(cls, data):
+
+        # stripe adds a deleted attribute if the Customer has been deleted upstream
+        if data.get("deleted"):
+            logger.warning(
+                f"This customer ({data.get('id')}) has been deleted upstream, in Stripe"
+            )
+            # add "deleted_on_stripe" key to the dict and set it to True
+            data["deleted_on_stripe"] = True
+        else:
+            data["deleted_on_stripe"] = False
+
         discount = data.get("discount")
         if discount:
             data["coupon_start"] = discount["start"]
@@ -1114,6 +1125,8 @@ class Customer(StripeModel):
         return payment_method
 
     def purge(self):
+        """Customers are soft deleted as deleted customers are still accessible by the
+        Stripe API and sync for all RelatedModels would fail"""
         try:
             self._api_delete()
         except InvalidRequestError as exc:
@@ -1124,6 +1137,10 @@ class Customer(StripeModel):
             else:
                 # The exception was raised for another reason, re-raise it
                 raise
+
+        # toggle the deleted_on_stripe flag on Customer to indicate it has been
+        # deleted upstream in Stripe
+        self.deleted_on_stripe = True
 
         if self.subscriber:
             # Delete the idempotency key used by Customer.create()
