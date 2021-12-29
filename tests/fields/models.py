3eafd7766dc97ee21c0bfdea8d297d24748a81c6
Arnav Choudhury <arnav13@gmail.com>
2021-12-29 21:54:20 +0530

    Added the TestCustomActionModel to test the Custom Admin Action
    Made TestCustomActionModel inherit from StripeModel so all sub-classes of StripeModel could also get tested easily.

--- tests/fields/models.py
+++ tests/fields/models.py
@@ -3,7 +3,14 @@
 from django.db import models
 
 from djstripe.fields import StripePercentField
+from djstripe.models import StripeModel
 
 
 class ExampleDecimalModel(models.Model):
     noval = StripePercentField()
+
+
+class TestCustomActionModel(StripeModel):
+    # for some reason having a FK here throws relation doesn't exist even though
+    # djstripe is also one of the installed apps in tests.settings
+    djstripe_owner_account = None
