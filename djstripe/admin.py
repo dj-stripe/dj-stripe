7c413e7b6c450cb064bc0fd24b1743704751ae1a
Arnav Choudhury <arnav13@gmail.com>
2022-01-13 00:40:12 +0530

    Added a SubscriptionScheduleInline Stackedinline model
    This admin model would be used with the Customer and Subscription Admin Views.

--- djstripe/admin.py
+++ djstripe/admin.py
@@ -224,6 +224,24 @@ class SubscriptionInline(admin.StackedInline):
     show_change_link = True
 
 
+class SubscriptionScheduleInline(admin.StackedInline):
+    """A TabularInline for use models.SubscriptionSchedule."""
+
+    model = models.SubscriptionSchedule
+    extra = 0
+    readonly_fields = ("id", "created", "djstripe_owner_account")
+    raw_id_fields = get_forward_relation_fields_for_model(model)
+    show_change_link = True
+
+    def __init__(self, parent_model, admin_site):
+        super().__init__(parent_model, admin_site)
+
+        # dynamically set fk_name as SubscriptionScheduleInline is used
+        # in CustomerAdmin as well as SubscriptionAdmin
+        if parent_model is models.Subscription:
+            self.fk_name = "subscription"
+
+
 class TaxIdInline(admin.TabularInline):
     """A TabularInline for use models.Subscription."""
 
