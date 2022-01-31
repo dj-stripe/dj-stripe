f24c78c609b2b10216d44c79c514fdb137dc651d
Dominik Bartenstein <db@zemtu.com>
2022-01-31 23:48:06 +0100

    See #1569: Webhook events for database-stored webhooks seem to be processed using the API key stored in environment
    Add missing api_key parameter

--- djstripe/models/webhooks.py
+++ djstripe/models/webhooks.py
@@ -312,7 +312,7 @@ class WebhookEventTrigger(models.Model):
         self.exception = ""
         self.traceback = ""
 
-        self.event = Event.process(self.json_body)
+        self.event = Event.process(self.json_body, api_key=self.stripe_trigger_account.default_api_key)
         self.processed = True
         if save:
             self.save()
