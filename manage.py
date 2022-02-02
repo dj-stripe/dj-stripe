ed834a6ea4be89d7ffa2953d235ed5c69ef867f3
Arnav Choudhury <arnav13@gmail.com>
2022-02-02 19:15:13 +0530

    Added VSCODE Debug Config using debugpy

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+import debugpy
+
 try:
     from django.core.management import execute_from_command_line
 except ImportError as exc:
@@ -9,6 +11,17 @@ except ImportError as exc:
         "Couldn't import Django. "
         "Run `poetry shell` to activate a virtual environment first."
     ) from exc
+
+
+# ? Attach a python debuger on 0.0.0.0:5678. Can be used directly in VScode Debugger
+try:
+    # each gunicorn worker will try to attach to the same host and port
+    # and that will throw an error as 1 process can be attached to a port
+    debugpy.listen(address=("0.0.0.0", 5678))
+    print("Django: Attached remote debugger")
+except RuntimeError as error:
+    # do nothing
+    print(f"Multiple workers trying to attach the remote debugger. {error}")
 
 
 def main():
