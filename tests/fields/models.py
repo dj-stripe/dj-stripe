15deceab7f41e607624dd723917bbb2c95d3ef1f
Arnav Choudhury <arnav13@gmail.com>
2022-03-26 15:25:22 +0530

    Added MockStripeClass to mock stripe_class attribute of TestCustomActionModel testing model

--- tests/fields/models.py
+++ tests/fields/models.py
@@ -10,10 +10,16 @@ class ExampleDecimalModel(models.Model):
     noval = StripePercentField()
 
 
+class MockStripeClass:
+    def retrieve(self):
+        return self
+
+
 class TestCustomActionModel(StripeModel):
     # for some reason having a FK here throws relation doesn't exist even though
     # djstripe is also one of the installed apps in tests.settings
     djstripe_owner_account = None
+    stripe_class = MockStripeClass
 
     # For Subscription model's custom action, _cancel
     def cancel(self, at_period_end: bool = False, **kwargs):
