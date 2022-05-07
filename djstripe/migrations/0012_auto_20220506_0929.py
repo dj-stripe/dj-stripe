812d76f388ec58487f875d9f9e1306829c6fde9b
Arnav Choudhury <arnav13@gmail.com>
2022-05-07 17:28:18 +0530

    Formatting fixes

--- djstripe/migrations/0012_auto_20220506_0929.py
+++ djstripe/migrations/0012_auto_20220506_0929.py
@@ -6,18 +6,26 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('djstripe', '0011_remove_subscriptionschedule_billing_thresholds_and_more'),
+        ("djstripe", "0011_remove_subscriptionschedule_billing_thresholds_and_more"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='event',
-            name='api_version',
-            field=models.CharField(blank=True, help_text='the API version at which the event data was rendered. Blank for old entries only, all new entries will have this value', max_length=64),
+            model_name="event",
+            name="api_version",
+            field=models.CharField(
+                blank=True,
+                help_text="the API version at which the event data was rendered. Blank for old entries only, all new entries will have this value",
+                max_length=64,
+            ),
         ),
         migrations.AlterField(
-            model_name='webhookendpoint',
-            name='api_version',
-            field=models.CharField(blank=True, help_text='The API version events are rendered as for this webhook endpoint.', max_length=64),
+            model_name="webhookendpoint",
+            name="api_version",
+            field=models.CharField(
+                blank=True,
+                help_text="The API version events are rendered as for this webhook endpoint.",
+                max_length=64,
+            ),
         ),
     ]
