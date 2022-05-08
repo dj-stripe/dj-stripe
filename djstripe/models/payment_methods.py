a2e264b357b934e9d349910e5fdf5f03b00df402
Arnav Choudhury <arnav13@gmail.com>
2022-05-08 23:29:44 +0530

    Added SourceTransaction.api_retrieve() method

--- djstripe/models/payment_methods.py
+++ djstripe/models/payment_methods.py
@@ -867,6 +867,27 @@ class SourceTransaction(StripeModel):
             api_key=api_key, **kwargs
         ).auto_paging_iter()
 
+    def api_retrieve(self, api_key=None, stripe_account=None):
+        """
+        Call the stripe API's retrieve operation for this model.
+
+        :param api_key: The api key to use for this request. \
+            Defaults to djstripe_settings.STRIPE_SECRET_KEY.
+        :type api_key: string
+        :param stripe_account: The optional connected account \
+            for which this request is being made.
+        :type stripe_account: string
+        """
+        # Prefer passed in stripe_account if set.
+        if not stripe_account:
+            stripe_account = self._get_stripe_account_id(api_key)
+
+        for source_trx in SourceTransaction.api_list(
+            id=self.source.id, api_key=api_key, stripe_account=stripe_account
+        ):
+            if source_trx.id == self.id:
+                return source_trx
+
 
 class PaymentMethod(StripeModel):
     """
