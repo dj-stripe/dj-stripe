045e2354b53e1b9cd2a426355bae5711beb988e7
Arnav Choudhury <arnav13@gmail.com>
2022-05-09 22:56:51 +0530

    Overrode admin.ModelAdmin.get_urls()
    This was done to re-use django's built in admin view
    functionality which is what admin actions are.
    
    get_urls() add the provided url as a url for the Admin by default.

--- djstripe/urls.py
+++ djstripe/urls.py
@@ -28,9 +28,4 @@ urlpatterns = [
         views.ProcessWebhookView.as_view(),
         name="djstripe_webhook_by_uuid",
     ),
-    path(
-        "action/<str:action_name>/<str:model_name>/",
-        admin_views.ConfirmCustomAction.as_view(),
-        name="djstripe_custom_action",
-    ),
 ]
