6715c6de258d48722f0944b2a92ff83594ff5eaa
Arnav Choudhury <arnav13@gmail.com>
2022-06-24 15:39:57 +0530

    Added Order.submit() method

--- djstripe/models/orders.py
+++ djstripe/models/orders.py
@@ -163,3 +163,27 @@ class Order(StripeModel):
         return self.stripe_class.reopen(
             self.id, api_key=api_key, stripe_account=stripe_account, **kwargs
         )
+
+    def submit(self, api_key=None, stripe_account=None, **kwargs):
+        """
+        Submitting an Order transitions the status to processing and creates a PaymentIntent object
+        so the order can be paid.
+        If the Order has an amount_total of 0, no PaymentIntent object will be created.
+        Once the order is submitted, its contents cannot be changed,
+        unless the reopen method is called.
+
+        :param api_key: The api key to use for this request. \
+            Defaults to djstripe_settings.STRIPE_SECRET_KEY.
+        :type api_key: string
+        :param stripe_account: The optional connected account \
+            for which this request is being made.
+        :type stripe_account: string
+        """
+        api_key = api_key or self.default_api_key
+        # Prefer passed in stripe_account if set.
+        if not stripe_account:
+            stripe_account = self._get_stripe_account_id(api_key)
+
+        return self.stripe_class.submit(
+            self.id, api_key=api_key, stripe_account=stripe_account, **kwargs
+        )
