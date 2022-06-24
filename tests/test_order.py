e9d7a09ef9dee848ddcdaf9877d58f1b3a0f993c
Arnav Choudhury <arnav13@gmail.com>
2022-06-24 15:36:30 +0530

    Added Order.cancel() method

--- tests/test_order.py
+++ tests/test_order.py
@@ -10,6 +10,7 @@ from django.test import TestCase
 
 from djstripe.enums import OrderStatus
 from djstripe.models import Order
+from djstripe.settings import djstripe_settings
 
 from . import (
     FAKE_ACCOUNT,
@@ -23,6 +24,7 @@ from . import (
     FAKE_ORDER_WITHOUT_CUSTOMER_WITHOUT_PAYMENT_INTENT,
     FAKE_PAYMENT_INTENT_I,
     FAKE_PAYMENT_METHOD_I,
+    FAKE_PLATFORM_ACCOUNT,
     FAKE_PRODUCT,
     FAKE_SUBSCRIPTION,
     AssertStripeFksMixin,
@@ -251,3 +253,85 @@ class TestOrderStr:
         ):
 
             assert str(order) == f"Placed on 07/10/2019 ({order_status})"
+
+
+class TestOrderMethods:
+    @pytest.mark.parametrize("stripe_account", (None, "acct_fakefakefakefake001"))
+    @pytest.mark.parametrize(
+        "api_key, expected_api_key",
+        (
+            (None, djstripe_settings.STRIPE_SECRET_KEY),
+            ("sk_fakefakefake01", "sk_fakefakefake01"),
+        ),
+    )
+    @patch(
+        "stripe.BalanceTransaction.retrieve",
+        return_value=deepcopy(FAKE_BALANCE_TRANSACTION),
+        autospec=True,
+    )
+    @patch(
+        "stripe.Subscription.retrieve",
+        return_value=deepcopy(FAKE_SUBSCRIPTION),
+        autospec=True,
+    )
+    @patch("stripe.Charge.retrieve", return_value=deepcopy(FAKE_CHARGE), autospec=True)
+    @patch(
+        "stripe.PaymentMethod.retrieve",
+        return_value=deepcopy(FAKE_PAYMENT_METHOD_I),
+        autospec=True,
+    )
+    @patch(
+        "stripe.PaymentIntent.retrieve",
+        return_value=deepcopy(FAKE_PAYMENT_INTENT_I),
+        autospec=True,
+    )
+    @patch(
+        "stripe.Product.retrieve", return_value=deepcopy(FAKE_PRODUCT), autospec=True
+    )
+    @patch(
+        "stripe.Customer.retrieve", return_value=deepcopy(FAKE_CUSTOMER), autospec=True
+    )
+    @patch(
+        "stripe.Invoice.retrieve", return_value=deepcopy(FAKE_INVOICE), autospec=True
+    )
+    @patch(
+        "stripe.Order.cancel",
+    )
+    def test_cancel(
+        self,
+        order_cancel_mock,
+        invoice_retrieve_mock,
+        customer_retrieve_mock,
+        product_retrieve_mock,
+        payment_intent_retrieve_mock,
+        paymentmethod_card_retrieve_mock,
+        charge_retrieve_mock,
+        subscription_retrieve_mock,
+        balance_transaction_retrieve_mock,
+        api_key,
+        expected_api_key,
+        stripe_account,
+    ):
+        fake_order = deepcopy(FAKE_ORDER_WITH_CUSTOMER_WITH_PAYMENT_INTENT)
+
+        fake_order_cancel = deepcopy(FAKE_ORDER_WITH_CUSTOMER_WITH_PAYMENT_INTENT)
+        fake_order_cancel["status"] = "canceled"
+        order_cancel_mock.return_value = fake_order_cancel
+
+        order = Order.sync_from_stripe_data(fake_order)
+
+        # Cancel the order
+        cancelled_order = order.cancel(
+            api_key=api_key,
+            stripe_account=stripe_account,
+        )
+
+        # assert Order got cancelled
+        assert cancelled_order["status"] == "canceled"
+
+        # assert cancel called with the correct kwargs
+        Order.stripe_class.cancel.assert_called_once_with(
+            "order_fakefakefakefakefake0001",
+            api_key=expected_api_key,
+            stripe_account=stripe_account or FAKE_PLATFORM_ACCOUNT["id"],
+        )
