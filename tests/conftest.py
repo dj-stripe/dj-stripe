306093ef5ea99511d92a4d9d1a77a49ac4b92c08
Arnav Choudhury <arnav13@gmail.com>
2022-10-25 22:58:18 +0530

    Added Account fixtures using the Actual Stripe API

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,11 @@ import os
 
 import pytest
 import stripe
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from stripe.error import InvalidRequestError, PermissionError
+
+from djstripe import models
 
 from . import FAKE_CUSTOMER, FAKE_PLATFORM_ACCOUNT
 
@@ -74,3 +78,118 @@ def fake_user():
 def fake_customer(fake_user):
     customer = FAKE_CUSTOMER.create_for_user(fake_user)
     return customer
+
+
+# Stripe Account Fixtures
+@pytest.fixture
+def standard_account_fixture(django_db_setup, django_db_blocker, configure_settings):
+    """Create Standard Stripe Account."""
+    # See: https://pytest-django.readthedocs.io/en/latest/database.html#populate-the-test-database-if-you-don-t-use-transactional-or-live-server
+    with django_db_blocker.unblock():
+
+        # setup_stuff
+        account_json = models.Account._api_create(
+            type="standard",
+            country="US",
+            email="jenny.standard.rosen@example.com",
+            api_key=settings.STRIPE_SECRET_KEY,
+        )
+        account_instance = models.Account.sync_from_stripe_data(
+            account_json,
+            api_key=settings.STRIPE_SECRET_KEY,
+        )
+
+        yield account_json, account_instance
+
+        # teardown_stuff
+        try:
+            # try to delete
+            account_instance._api_delete(api_key=settings.STRIPE_SECRET_KEY)
+        except (InvalidRequestError, PermissionError):
+            pass
+
+
+@pytest.fixture
+def custom_account_fixture(django_db_setup, django_db_blocker, configure_settings):
+    """Create Custom Stripe Account."""
+    # See: https://pytest-django.readthedocs.io/en/latest/database.html#populate-the-test-database-if-you-don-t-use-transactional-or-live-server
+    with django_db_blocker.unblock():
+
+        # setup_stuff
+        account_json = models.Account._api_create(
+            type="custom",
+            country="US",
+            email="jenny.custom.rosen@example.com",
+            capabilities={
+                "card_payments": {"requested": True},
+                "transfers": {"requested": True},
+            },
+            api_key=settings.STRIPE_SECRET_KEY,
+        )
+        account_instance = models.Account.sync_from_stripe_data(
+            account_json,
+            api_key=settings.STRIPE_SECRET_KEY,
+        )
+
+        yield account_json, account_instance
+
+        # teardown_stuff
+        try:
+            # try to delete
+            account_instance._api_delete(api_key=settings.STRIPE_SECRET_KEY)
+        except (InvalidRequestError, PermissionError):
+            pass
+
+
+@pytest.fixture
+def custom_account_func_fixture(django_db_setup, django_db_blocker, configure_settings):
+    """
+    Same as custom_account_fixture but functional.
+
+    This is useful for tests involving rejecting and deleting instances.
+    """
+    # See: https://pytest-django.readthedocs.io/en/latest/database.html#populate-the-test-database-if-you-don-t-use-transactional-or-live-server
+    with django_db_blocker.unblock():
+
+        # setup_stuff
+        account_json = models.Account._api_create(
+            type="custom",
+            country="US",
+            email="jenny.custom.rosen@example.com",
+            capabilities={
+                "card_payments": {"requested": True},
+                "transfers": {"requested": True},
+            },
+            api_key=settings.STRIPE_SECRET_KEY,
+        )
+        account_instance = models.Account.sync_from_stripe_data(
+            account_json,
+            api_key=settings.STRIPE_SECRET_KEY,
+        )
+
+        yield account_json, account_instance
+
+        # teardown_stuff
+        try:
+            # try to delete
+            account_instance._api_delete(api_key=settings.STRIPE_SECRET_KEY)
+        except (InvalidRequestError, PermissionError):
+            pass
+
+
+@pytest.fixture
+def platform_account_fixture(django_db_setup, django_db_blocker, configure_settings):
+    """Retrieve Platform Stripe Account."""
+    # See: https://pytest-django.readthedocs.io/en/latest/database.html#populate-the-test-database-if-you-don-t-use-transactional-or-live-server
+    with django_db_blocker.unblock():
+
+        # setup_stuff
+        account_json = stripe.Account.retrieve(
+            api_key=settings.STRIPE_SECRET_KEY,
+        )
+        account_instance = models.Account.sync_from_stripe_data(
+            account_json,
+            api_key=settings.STRIPE_SECRET_KEY,
+        )
+
+        yield account_json, account_instance
