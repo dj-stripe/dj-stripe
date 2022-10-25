1479c0b7c45a5585be6e5ac7b50260f67c3c1f82
Arnav Choudhury <arnav13@gmail.com>
2022-10-25 23:11:11 +0530

    Added pytest hook to run all tests using mocked Stripe API by default
    Tests using the actual Stripe API will be run iff the marker stripe_api is
    explicitly passed on the command line.

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,6 +28,23 @@ class CreateAccountMixin:
         FAKE_PLATFORM_ACCOUNT.create()
 
 
+def pytest_collection_modifyitems(items, config):
+    """Override Pytest config at run-time to run tests using Stripe API only if explictly specified using `-m stripe_api`"""
+    # get passed in markers
+    markexpr = config.getoption("markexpr")
+    if markexpr:
+        # allow passed in markers to run
+        config.option.markexpr = markexpr
+    else:
+        # skip running `stripe_api` marked tests unless explictly specified
+        for item in items:
+            if "stripe_api" in item.keywords:
+                # add message to let user know how to run tests using Stripe API
+                item.add_marker(
+                    pytest.mark.skip(reason="need -m stripe_api option to run")
+                )
+
+
 @pytest.fixture
 def configure_settings(settings):
     settings.STRIPE_TEST_SECRET_KEY = settings.STRIPE_SECRET_KEY = os.environ.get(
