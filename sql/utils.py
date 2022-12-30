eb48c965270bf48d41b52da0836bd48ba9a06f5e
Arnav Choudhury <arnav13@gmail.com>
2022-12-30 16:09:46 +0530

    Added a helper method get_sql_for_connection
    get_sql_for_connection is used to pick the correct
    collection of SQL statements that need to run for the
    given SQL engine and migration direction.

--- sql/utils.py
+++ sql/utils.py
@@ -159,3 +159,20 @@ CUSTOM_SQL = {
             """,
     },
 }
+
+
+def get_sql_for_connection(schema_editor, direction: str) -> str:
+    """
+    Returns the vendor and the collection of SQL Statements depending on:
+
+    1. The SQL Engine
+    2. Direction of Migrations: forward or Backward
+    """
+    vendor = schema_editor.connection.vendor
+    try:
+        return vendor, CUSTOM_SQL[vendor][direction]
+    except KeyError as error:
+        # In case it's oracle or some other django supported db that we do not support yet.
+        raise RuntimeError(
+            f"We currently do not support {vendor}. Please open an issue at https://github.com/dj-stripe/dj-stripe/issues/new?assignees=&labels=discussion&template=feature-or-enhancement-proposal.md&title= if you'd like it supported.",
+        ) from error
