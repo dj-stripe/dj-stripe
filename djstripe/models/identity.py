0feabe963e12973ecd144462d6c3d1d6f134c69c
Arnav Choudhury <arnav13@gmail.com>
2023-05-12 15:09:31 +0530

    Added missing class docstrings

--- djstripe/models/identity.py
+++ djstripe/models/identity.py
@@ -8,6 +8,15 @@ from .base import StripeModel
 
 class VerificationSession(StripeModel):
     """
+    A VerificationSession guides you through the process of collecting and verifying
+    the identities of your users. It contains details about the type of verification,
+    such as what verification check to perform. Only create one VerificationSession
+    for each verification in your system.
+
+    A VerificationSession transitions through multiple statuses throughout its
+    lifetime as it progresses through the verification flow. The VerificationSession
+    contains the user's verified data after verification checks are complete.
+
     Stripe documentation: https://stripe.com/docs/api/identity/verification_sessions
     """
 
@@ -29,6 +38,15 @@ class VerificationSession(StripeModel):
 
 class VerificationReport(StripeModel):
     """
+    A VerificationReport is the result of an attempt to collect and verify data from a user.
+    The collection of verification checks performed is determined from the type and options parameters
+    used. You can find the result of each verification check performed in the appropriate sub-resource:
+    document, id_number, selfie.
+
+    Each VerificationReport contains a copy of any data collected by the user as well as
+    reference IDs which can be used to access collected images through the FileUpload API.
+    To configure and create VerificationReports, use the VerificationSession API.
+
     Stripe documentation: https://stripe.com/docs/api/identity/verification_reports
     """
 
