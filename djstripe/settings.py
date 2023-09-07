2f7eaecb2e86c0cdb547aba21c048204330865dd
Arnav Choudhury <arnav13@gmail.com>
2023-09-07 10:45:33 +0530

    Removed depreacted DJSTRIPE_WEBHOOK_VALIDATION setting

--- djstripe/settings.py
+++ djstripe/settings.py
@@ -70,10 +70,6 @@ class DjstripeSettings:
     def decimal_places(self):
         return 12
 
-    @property
-    def WEBHOOK_VALIDATION(self):
-        return getattr(settings, "DJSTRIPE_WEBHOOK_VALIDATION", "verify_signature")
-
     # Webhook event callbacks allow an application to take control of what happens
     # when an event from Stripe is received.  One suggestion is to put the event
     # onto a task queue (such as celery) for asynchronous processing.
