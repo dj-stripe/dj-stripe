3edd112d7456fadbc9e0bea9504b05a654f77537
Jerome Leclanche <jerome@leclan.ch>
2024-03-20 11:47:03 +0100

    Add document expand_fields on VerificationReport
    Refs #1529

--- djstripe/models/identity.py
+++ djstripe/models/identity.py
@@ -33,6 +33,7 @@ class VerificationReport(StripeModel):
     """
 
     stripe_class = stripe.identity.VerificationReport
+    expand_fields = ["document.dob", "document.expiration_date"]
 
     document = JSONField(null=True, blank=True)
     id_number = JSONField(null=True, blank=True)
