ed74bb6d69f04af45b64e3ff8efa4e957cae60b7
Abe Hanoka <abe@habet.dev>
2024-06-19 16:40:38 -0400

    add promotion code migraition

--- djstripe/migrations/0013_2_9.py
+++ djstripe/migrations/0013_2_9.py
@@ -3,6 +3,7 @@
 import uuid
 
 from django.db import migrations, models
+from django.conf import settings
 
 import djstripe.enums
 import djstripe.fields
@@ -943,5 +944,52 @@ class Migration(migrations.Migration):
         ),
         migrations.DeleteModel(
             name="Order",
+        ),
+        migrations.CreateModel(
+            name="PromotionCode",
+            fields=[
+                ("djstripe_created", models.DateTimeField(auto_now_add=True)),
+                ("djstripe_updated", models.DateTimeField(auto_now=True)),
+                ("stripe_data", djstripe.fields.JSONField(default=dict)),
+                (
+                    "djstripe_id",
+                    models.BigAutoField(
+                        primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("id", djstripe.fields.StripeIdField(max_length=255, unique=True)),
+                (
+                    "livemode",
+                    models.BooleanField(
+                        blank=True,
+                        default=None,
+                        help_text="Null here indicates that the livemode status is unknown or was previously unrecorded. Otherwise, this field indicates whether this record comes from Stripe test mode or live mode operation.",
+                        null=True,
+                    ),
+                ),
+                ("created", djstripe.fields.StripeDateTimeField(blank=True, null=True)),
+                ("metadata", djstripe.fields.JSONField(blank=True, null=True)),
+                (
+                    "description",
+                    models.TextField(
+                        blank=True, help_text="A description of this object.", null=True
+                    ),
+                ),
+                (
+                    "djstripe_owner_account",
+                    djstripe.fields.StripeForeignKey(
+                        blank=True,
+                        help_text="The Stripe Account this object belongs to.",
+                        null=True,
+                        on_delete=models.deletion.CASCADE,
+                        to="djstripe.account",
+                        to_field=settings.DJSTRIPE_FOREIGN_KEY_TO_FIELD,
+                    ),
+                ),
+            ],
+            options={
+                "get_latest_by": "created",
+                "abstract": False,
+            },
         ),
     ]
