f87357ca644477e76eb7c6ded0dc966ff57e6b39
Abe Hanoka <abe@habet.dev>
2024-07-24 22:14:26 -0400

    add migration for decimal places and max digits

--- djstripe/migrations/0013_2_9.py
+++ djstripe/migrations/0013_2_9.py
@@ -992,4 +992,214 @@ class Migration(migrations.Migration):
                 "abstract": False,
             },
         ),
+        migrations.AlterField(
+            model_name="charge",
+            name="amount",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14
+            ),
+        ),
+        migrations.AlterField(
+            model_name="charge",
+            name="amount_captured",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="charge",
+            name="amount_refunded",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14
+            ),
+        ),
+        migrations.AlterField(
+            model_name="charge",
+            name="application_fee_amount",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                blank=True, decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="coupon",
+            name="amount_off",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                blank=True, decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="invoice",
+            name="amount_due",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14
+            ),
+        ),
+        migrations.AlterField(
+            model_name="invoice",
+            name="amount_paid",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="invoice",
+            name="amount_remaining",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="invoice",
+            name="application_fee_amount",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                blank=True, decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="invoice",
+            name="subtotal",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14
+            ),
+        ),
+        migrations.AlterField(
+            model_name="invoice",
+            name="tax",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                blank=True, decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="invoice",
+            name="total",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14
+            ),
+        ),
+        migrations.AlterField(
+            model_name="invoiceitem",
+            name="amount",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14
+            ),
+        ),
+        migrations.AlterField(
+            model_name="invoiceitem",
+            name="unit_amount_decimal",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                blank=True, decimal_places=12, max_digits=24, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="lineitem",
+            name="unit_amount_excluding_tax",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                blank=True, decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="payout",
+            name="amount",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14
+            ),
+        ),
+        migrations.AlterField(
+            model_name="plan",
+            name="amount",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                blank=True, decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="plan",
+            name="amount_decimal",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                blank=True, decimal_places=12, max_digits=24, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="price",
+            name="unit_amount_decimal",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                blank=True, decimal_places=12, max_digits=24, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="source",
+            name="amount",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                blank=True, decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="sourcetransaction",
+            name="amount",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                blank=True, decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="transfer",
+            name="amount",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14
+            ),
+        ),
+        migrations.AlterField(
+            model_name="transfer",
+            name="amount_reversed",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                blank=True, decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="upcominginvoice",
+            name="amount_due",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14
+            ),
+        ),
+        migrations.AlterField(
+            model_name="upcominginvoice",
+            name="amount_paid",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="upcominginvoice",
+            name="amount_remaining",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="upcominginvoice",
+            name="application_fee_amount",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                blank=True, decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="upcominginvoice",
+            name="subtotal",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14
+            ),
+        ),
+        migrations.AlterField(
+            model_name="upcominginvoice",
+            name="tax",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                blank=True, decimal_places=2, max_digits=14, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="upcominginvoice",
+            name="total",
+            field=djstripe.fields.StripeDecimalCurrencyAmountField(
+                decimal_places=2, max_digits=14
+            ),
+        ),
     ]
