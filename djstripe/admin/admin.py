95810b175c84c95fa67648e595610d80baacd9f3
Jerome Leclanche <jerome@leclan.ch>
2024-08-10 19:11:49 +0200

    Remove 'active' field from PromotionCodeAdmin

--- djstripe/admin/admin.py
+++ djstripe/admin/admin.py
@@ -516,9 +516,9 @@ class ProductAdmin(StripeModelAdmin):
 
 @admin.register(models.PromotionCode)
 class PromotionCodeAdmin(StripeModelAdmin):
-    list_display = ("code", "active", "created", "times_redeemed", "max_redemptions")
+    list_display = ("code", "created", "times_redeemed", "max_redemptions")
     search_fields = ("code",)
-    list_filter = ("active", "created")
+    list_filter = ("created",)
 
 
 @admin.register(models.Refund)
