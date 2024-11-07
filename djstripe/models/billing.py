ab3056fb0fccae1a5e033f1d376b2dec17559320
Jerome Leclanche <jerome@leclan.ch>
2024-11-07 11:50:02 +0100

    Fix TaxId retrieval for account-level IDs
    Fixes #2106

--- djstripe/models/billing.py
+++ djstripe/models/billing.py
@@ -2688,8 +2688,7 @@ class TaxId(StripeModel):
         if not stripe_account:
             stripe_account = self._get_stripe_account_id(api_key)
 
-        return stripe.Customer.retrieve_tax_id(
-            customer=self.customer.id,
+        return stripe.TaxId.retrieve(
             id=self.id,
             api_key=api_key,
             expand=self.expand_fields,
