40758ed687ef17ee95071d18e2aced93b217276e
Jerome Leclanche <jerome@leclan.ch>
2024-11-18 23:32:02 +0100

    Remove api overrides to TaxId
    This was valid in a previous version of the SDK, but not anymore.
    
    (cherry picked from commit a5df49c38123478f9ef756eed1131fad41ea9426)

--- djstripe/models/billing.py
+++ djstripe/models/billing.py
@@ -27,7 +27,6 @@ from ..managers import SubscriptionManager
 from ..settings import djstripe_settings
 from ..utils import QuerySetMock, get_friendly_currency_amount, get_id_from_stripe_data
 from .base import StripeModel
-from .core import Customer
 
 logger = logging.getLogger(__name__)
 
@@ -2656,69 +2655,6 @@ class TaxId(StripeModel):
 
     class Meta(StripeModel.Meta):
         verbose_name = "Tax ID"
-
-    @classmethod
-    def _api_create(cls, api_key=djstripe_settings.STRIPE_SECRET_KEY, **kwargs):
-        """
-        Call the stripe API's create operation for this model.
-
-        :param api_key: The api key to use for this request. \
-            Defaults to djstripe_settings.STRIPE_SECRET_KEY.
-        :type api_key: string
-        """
-
-        if not kwargs.get("id"):
-            raise KeyError("Customer Object ID is missing")
-
-        try:
-            Customer.objects.get(id=kwargs["id"])
-        except Customer.DoesNotExist:
-            raise
-
-        return stripe.Customer.create_tax_id(api_key=api_key, **kwargs)
-
-    def api_retrieve(self, api_key=None, stripe_account=None):
-        """
-        Call the stripe API's retrieve operation for this model.
-        :param api_key: The api key to use for this request. \
-            Defaults to djstripe_settings.STRIPE_SECRET_KEY.
-        :type api_key: string
-        :param stripe_account: The optional connected account \
-            for which this request is being made.
-        :type stripe_account: string
-        """
-        api_key = api_key or self.default_api_key
-
-        # Prefer passed in stripe_account if set.
-        if not stripe_account:
-            stripe_account = self._get_stripe_account_id(api_key)
-
-        return stripe.TaxId.retrieve(
-            id=self.id,
-            api_key=api_key,
-            expand=self.expand_fields,
-            stripe_account=stripe_account,
-            stripe_version=djstripe_settings.STRIPE_API_VERSION,
-        )
-
-    @classmethod
-    def api_list(cls, api_key=djstripe_settings.STRIPE_SECRET_KEY, **kwargs):
-        """
-        Call the stripe API's list operation for this model.
-        :param api_key: The api key to use for this request. \
-            Defaults to djstripe_settings.STRIPE_SECRET_KEY.
-        :type api_key: string
-        See Stripe documentation for accepted kwargs for each object.
-        :returns: an iterator over all items in the query
-        """
-        # Update kwargs with `expand` param
-        kwargs = cls.get_expand_params(api_key, **kwargs)
-
-        return stripe.Customer.list_tax_ids(
-            api_key=api_key,
-            stripe_version=djstripe_settings.STRIPE_API_VERSION,
-            **kwargs,
-        ).auto_paging_iter()
 
     @property
     def verification(self) -> dict | None:
