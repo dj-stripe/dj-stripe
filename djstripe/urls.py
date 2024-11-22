1dfe2af9561242db9ff27e4d6e903d04bbd255f1
Jerome Leclanche <jerome@leclan.ch>
2024-11-22 11:01:01 +0100

    Respect APPEND_SLASH in the webhook URLs
    (cherry picked from commit 00a078bb2c62ba552fb02289d3a97623ae8ccbd5)

--- djstripe/urls.py
+++ djstripe/urls.py
@@ -8,16 +8,22 @@ Wire this into the root URLConf this way::
     # Call to 'djstripe.urls' and 'namespace' must stay as is
 """
 
+from django.conf import settings
 from django.urls import path
 
 from . import views
 
 app_name = "djstripe"
 
+
+def trailing_slash(path: str) -> str:
+    return path + "/" if settings.APPEND_SLASH else path
+
+
 urlpatterns = [
     # Webhook
     path(
-        "webhook/<uuid:uuid>/",
+        trailing_slash("webhook/<uuid:uuid>"),
         views.ProcessWebhookView.as_view(),
         name="djstripe_webhook_by_uuid",
     ),
